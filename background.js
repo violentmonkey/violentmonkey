16d7b0ae0d3443ae30ba1cbb3bf0669781873a00
Gerald <gera2ld@163.com>
2014-09-05 23:33:04 +0800

    fixed #16

--- background.js
+++ background.js
@@ -625,7 +625,7 @@ chrome.webRequest.onBeforeRequest.addListener(function(o){
 		var x=new XMLHttpRequest();
 		x.open('GET',o.url,false);
 		x.send();
-		if((!x.status||x.status==200)&&/^\s*[^<]/.test(x.responseText)) {
+		if((!x.status||x.status==200)&&x.responseText[0]=='/') {
 			if(o.tabId<0) chrome.tabs.create({url:chrome.extension.getURL('/confirm.html')+'?url='+encodeURIComponent(o.url)});
 			else chrome.tabs.get(o.tabId,function(t){
 				chrome.tabs.create({url:chrome.extension.getURL('/confirm.html')+'?url='+encodeURIComponent(o.url)+'&from='+encodeURIComponent(t.url)});
