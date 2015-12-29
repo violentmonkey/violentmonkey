d9046cf739a8474230156b0b2890b9f505ec3711
Gerald <gera2ld@163.com>
2015-12-29 10:13:22 +0800

    Add version in about tab, close #35

--- src/options/views/tab-about.js
+++ src/options/views/tab-about.js
@@ -2,4 +2,9 @@ var AboutTab = BaseView.extend({
   el: '#tab',
   name: 'about',
   templateUrl: '/options/templates/tab-about.html',
+  _render: function () {
+    this.$el.html(this.templateFn({
+      version: chrome.app.getDetails().version,
+    }));
+  },
 });
