0361074665454de629a253058584a320a33443b6
Gerald <gera2ld@163.com>
2016-04-01 10:57:30 +0800

    fix(Gulpfile): fix template cache

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ const paths = {
   ],
   copy: [
     'src/*.js',
+    '!src/cache.js',
     'src/public/**',
     'src/*/*.html',
     'src/*/*.css',
--- src/common.js
+++ src/common.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var _ = window._ || {};
 _.i18n = chrome.i18n.getMessage;
 
