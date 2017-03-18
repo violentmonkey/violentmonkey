14a48e0eeea80335cb9716bd4aeac242227eb200
Gerald <gera2ld@163.com>
2017-03-18 18:20:10 +0800

    fix(feature): fix potential memory leak

--- src/options/utils/features.js
+++ src/options/utils/features.js
@@ -15,6 +15,7 @@ var features = _.options.get(key);
 if (!features || !features.data) features = {
   data: {},
 };
+var items = {};
 
 exports.reset = function (version) {
   if (features.version !== version) {
@@ -25,22 +26,48 @@ exports.reset = function (version) {
   }
 };
 
+function getContext(el, value) {
+  function onFeatureClick(_e) {
+    features.data[value] = 1;
+    _.options.set(key, features);
+    el.classList.remove('feature');
+    el.removeEventListener('click', onFeatureClick, false);
+  }
+  function clear() {
+    el.removeEventListener('click', onFeatureClick, false);
+  }
+  function reset() {
+    clear();
+    if (!features.version || features.data[value]) return;
+    el.classList.add('feature');
+    el.addEventListener('click', onFeatureClick, false);
+  }
+  return {
+    el: el,
+    reset: reset,
+    clear: clear,
+  };
+}
+
 Vue.directive('feature', {
   bind: function (el, binding) {
-    function onFeatureClick(_e) {
-      features.data[value] = 1;
-      _.options.set(key, features);
-      el.classList.remove('feature');
-      el.removeEventListener('click', onFeatureClick, false);
-    }
-    function reset() {
-      if (!features.version || features.data[value]) return;
-      el.classList.add('feature');
-      el.removeEventListener('click', onFeatureClick, false);
-      el.addEventListener('click', onFeatureClick, false);
-    }
     var value = binding.value;
-    reset();
-    hooks && hooks.hook(reset);
+    var item = getContext(el, value);
+    var list = items[value] = items[value] || [];
+    list.push(item);
+    item.reset();
+    hooks && hooks.hook(item.reset);
+  },
+  unbind: function (el, binding) {
+    var list = items[binding.value];
+    if (list) {
+      var index = list.findIndex(function (item) {
+        return item.el === el;
+      });
+      if (~index) {
+        list[index].clear();
+        list.splice(index, 1);
+      }
+    }
   },
 });
