dbc384d33dba28f5ec8f22df05b289c252a14980
Gerald <gera2ld@163.com>
2017-03-19 12:04:24 +0800

    fix utils

--- src/common.js
+++ src/common.js
@@ -163,8 +163,10 @@ _.debounce = function (func, time) {
     func.apply(thisObj, args);
   }
   var timer;
-  return function (args) {
+  return function () {
     timer && clearTimeout(timer);
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) args.push(arguments[i]);
     timer = setTimeout(run, time, this, args);
   };
 };
