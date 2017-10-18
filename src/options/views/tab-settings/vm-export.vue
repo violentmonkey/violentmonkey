d88be14c3fe90618100d9f42e0173267fcee23d1
Gerald <gera2ld@163.com>
2017-10-18 23:37:44 +0800

    fix: allow Firefox user to download dataURL manually
    close #113, #243

--- src/options/views/tab-settings/vm-export.vue
+++ src/options/views/tab-settings/vm-export.vue
@@ -14,10 +14,19 @@
       <setting-check name="exportValues" />
       <span v-text="i18n('labelExportScriptData')"></span>
     </label>
+    <vl-modal transition="in-out" :visible="!!store.ffUrl" @close="store.ffUrl = null">
+      <div class="export-modal modal-content">
+        <a download="scripts.zip" :href="store.ffUrl">
+          Right click and save as<br />
+          <strong>scripts.zip</strong>
+        </a>
+      </div>
+    </vl-modal>
   </section>
 </template>
 
 <script>
+import VlModal from 'vueleton/lib/modal';
 import { sendMessage, getLocaleString } from 'src/common';
 import options from 'src/common/options';
 import { isFirefox } from 'src/common/ua';
@@ -28,13 +37,16 @@ import { store } from '../../utils';
  * Note:
  * - Firefox does not support multiline <select>
  */
+if (isFirefox) store.ffUrl = null;
 
 export default {
   components: {
     SettingCheck,
+    VlModal,
   },
   data() {
     return {
+      isFirefox,
       store,
       exporting: false,
       items: [],
@@ -115,7 +127,7 @@ function downloadBlob(blob) {
   if (isFirefox) {
     const reader = new FileReader();
     reader.onload = () => {
-      download(reader.result);
+      store.ffUrl = reader.result;
     };
     reader.readAsDataURL(blob);
   } else {
@@ -210,5 +222,8 @@ function exportData(selectedIds) {
       color: white;
     }
   }
+}
+.export-modal {
+  width: 13rem;
 }
 </style>
