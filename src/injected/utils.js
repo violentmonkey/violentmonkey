796cddd9f19b1b627a7f4e2071e96d766d76eef4
Tom Schuster <evilpies@gmail.com>
2017-10-22 13:32:40 +0200

    use src with Blob for injection

--- src/injected/utils.js
+++ src/injected/utils.js
@@ -12,9 +12,12 @@ export function postData(destId, data) {
 export function inject(code) {
   const script = document.createElement('script');
   const doc = document.body || document.documentElement;
-  script.textContent = code;
+  const blob = new Blob([code], { type: 'text/javascript' });
+  const url = URL.createObjectURL(blob);
+  script.src = url;
   doc.appendChild(script);
   try {
+    URL.revokeObjectURL(url);
     doc.removeChild(script);
   } catch (e) {
     // ignore if body is changed and script is detached
