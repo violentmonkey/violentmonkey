bb677596ef50b75892e9a9a1e42cda93c5a6a683
Gerald <gera2ld@163.com>
2018-11-05 10:02:52 +0800

    fix: show current editing script at the top

--- src/options/views/edit/index.vue
+++ src/options/views/edit/index.vue
@@ -1,31 +1,26 @@
 <template>
   <div class="edit frame flex flex-col fixed-full">
-    <div class="flex flex-wrap edit-header">
-      <h2 class="mr-2 ml-1" v-text="i18n('labelScriptEditor')"></h2>
-      <div class="flex-auto">
-        <div class="flex mx-1 my-1">
-          <div class="edit-hint flex-auto text-right ellipsis mr-1">
-            <a href="https://violentmonkey.github.io/2017/03/14/How-to-edit-scripts-with-your-favorite-editor/" target="_blank">How to edit with your favorite editor?</a>
-          </div>
-          <div class="edit-buttons">
-            <button v-text="i18n('buttonSave')" @click="save" :disabled="!canSave"></button>
-            <button v-text="i18n('buttonSaveClose')" @click="saveClose" :disabled="!canSave"></button>
-            <button v-text="i18n('buttonClose')" @click="close"></button>
-            <div class="text-red mt-1" v-if="tooLarge" v-text="i18n('warnScriptLongLines')"></div>
-          </div>
+    <div class="flex flex-wrap edit-header mx-1 my-1">
+      <div class="edit-info text-right ellipsis">
+        <strong v-text="i18n('labelEditing')"></strong>
+        <em v-text="scriptName"></em>
+      </div>
+      <div class="flex-auto flex">
+        <div class="edit-hint flex-auto text-right ellipsis mr-1">
+          <a href="https://violentmonkey.github.io/2017/03/14/How-to-edit-scripts-with-your-favorite-editor/" target="_blank">How to edit with your favorite editor?</a>
         </div>
-        <div class="flex mx-1">
-          <div class="edit-nav">
-            <div class="edit-nav-item" v-text="i18n('editNavCode')" :class="{active: nav === 'code'}" @click="nav = 'code'"></div>
-            <div class="edit-nav-item" v-text="i18n('editNavSettings')" :class="{active: nav === 'settings'}" @click="nav = 'settings'"></div>
-            <div class="edit-nav-item" v-text="i18n('editNavValues')" :class="{active: nav === 'values'}" @click="nav = 'values'"></div>
-          </div>
-          <div class="edit-info flex-auto text-right mt-1 ellipsis">
-            <span v-text="i18n('labelEditing')"></span>
-            <em v-text="scriptName"></em>
-          </div>
+        <div class="edit-buttons">
+          <button v-text="i18n('buttonSave')" @click="save" :disabled="!canSave"></button>
+          <button v-text="i18n('buttonSaveClose')" @click="saveClose" :disabled="!canSave"></button>
+          <button v-text="i18n('buttonClose')" @click="close"></button>
+          <div class="text-red mt-1" v-if="tooLarge" v-text="i18n('warnScriptLongLines')"></div>
         </div>
       </div>
+    </div>
+    <div class="edit-nav mx-1">
+      <div class="edit-nav-item" v-text="i18n('editNavCode')" :class="{active: nav === 'code'}" @click="nav = 'code'"></div>
+      <div class="edit-nav-item" v-text="i18n('editNavSettings')" :class="{active: nav === 'settings'}" @click="nav = 'settings'"></div>
+      <div class="edit-nav-item" v-text="i18n('editNavValues')" :class="{active: nav === 'values'}" @click="nav = 'values'"></div>
     </div>
     <div class="frame-block flex-auto pos-rel">
       <vm-code
