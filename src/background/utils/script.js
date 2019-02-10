467f4f3bec403ad8cf7342d82787dacb2571ecb7
divinity76 <divinity76@gmail.com>
2019-02-10 17:33:21 +0100

    formatting (ty travis)

--- src/background/utils/script.js
+++ src/background/utils/script.js
@@ -112,7 +112,7 @@ export function getNameURI(script) {
 }
 
 export function compareVersion(ver1, ver2) {
-  if(ver1 === undefined || ver2 === undefined) {
+  if (ver1 === undefined || ver2 === undefined) {
     return -1;
   }
   const parts1 = (ver1 || '').split('.');
