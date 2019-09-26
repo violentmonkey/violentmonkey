7edb583ca0780111b6946b90e2dbbdb32e08452b
tophf <tophf@gmx.com>
2019-09-26 17:59:11 +0300

    add a throttled forEachTab()

--- src/background/utils/message.js
+++ src/background/utils/message.js
@@ -11,15 +11,26 @@ export function notify(options) {
 }
 
 export function broadcast(data) {
-  browser.tabs.query({})
-  .then((tabs) => {
-    tabs.forEach((tab) => {
-      browser.tabs.sendMessage(tab.id, data)
-      .catch(noop);
-    });
+  forEachTab((tab) => {
+    browser.tabs.sendMessage(tab.id, data)
+    .catch(noop);
   });
 }
 
 export function sendMessageOrIgnore(...args) {
   return browser.runtime.sendMessage(...args).catch(noop);
+}
+
+export function forEachTab(callback) {
+  // we'll run the callback for each tab in a separate event loop cycle
+  // because hundreds of tabs would make our extension process unresponsive,
+  // the same process used by our own pages like the background page, dashboard, or popups
+  browser.tabs.query({})
+  .then(function throttle(tabs) {
+    const tab = tabs.shift();
+    if (tab) {
+      callback(tab);
+      setTimeout(throttle, 0, tabs);
+    }
+  });
 }
