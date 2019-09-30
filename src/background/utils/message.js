6efd43a0671b81181e007c53852d32569c4e1d9f
tophf <tophf@gmx.com>
2019-09-30 09:44:45 +0300

    use async/await

--- src/background/utils/message.js
+++ src/background/utils/message.js
@@ -1,6 +1,4 @@
-import { i18n, defaultImage, noop } from '#/common';
-
-const THROTTLED_BATCH_SIZE = 20;
+import { defaultImage, i18n, noop } from '#/common';
 
 export function notify(options) {
   browser.notifications.create(options.id || 'ViolentMonkey', {
@@ -23,17 +21,15 @@ export function sendMessageOrIgnore(...args) {
   return browser.runtime.sendMessage(...args).catch(noop);
 }
 
-export function forEachTab(callback) {
-  // we'll run the callback for each tab in a separate event loop cycle
-  // because hundreds of tabs would make our extension process unresponsive,
-  // the same process used by our own pages like the background page, dashboard, or popups
-  browser.tabs.query({})
-  .then(function throttle(tabs) {
-    const num = Math.min(THROTTLED_BATCH_SIZE, tabs.length);
-    for (let i = 0; i < num; i += 1) callback(tabs[i]);
-    if (tabs.length > THROTTLED_BATCH_SIZE) {
-      tabs.splice(0, THROTTLED_BATCH_SIZE);
-      setTimeout(throttle, 0, tabs);
-    }
-  });
+export async function forEachTab(callback) {
+  const tabs = await browser.tabs.query({});
+  let i = 0;
+  for (const tab of tabs) {
+    callback(tab);
+    i += 1;
+    // we'll run at most this many tabs in one event loop cycle
+    // because hundreds of tabs would make our extension process unresponsive,
+    // the same process used by our own pages like the background page, dashboard, or popups
+    if (i % 20 === 0) await new Promise(setTimeout);
+  }
 }
