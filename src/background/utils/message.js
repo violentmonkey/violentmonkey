5cf300ae1c23516bb2d05bdff6b8e538995a40b0
tophf <tophf@gmx.com>
2019-09-30 09:25:16 +0300

    throttle every 20th API call

--- src/background/utils/message.js
+++ src/background/utils/message.js
@@ -1,5 +1,7 @@
 import { i18n, defaultImage, noop } from '#/common';
 
+const THROTTLED_BATCH_SIZE = 20;
+
 export function notify(options) {
   browser.notifications.create(options.id || 'ViolentMonkey', {
     type: 'basic',
@@ -27,9 +29,10 @@ export function forEachTab(callback) {
   // the same process used by our own pages like the background page, dashboard, or popups
   browser.tabs.query({})
   .then(function throttle(tabs) {
-    const tab = tabs.shift();
-    if (tab) {
-      callback(tab);
+    const num = Math.min(THROTTLED_BATCH_SIZE, tabs.length);
+    for (let i = 0; i < num; i += 1) callback(tabs[i]);
+    if (tabs.length > THROTTLED_BATCH_SIZE) {
+      tabs.splice(0, THROTTLED_BATCH_SIZE);
       setTimeout(throttle, 0, tabs);
     }
   });
