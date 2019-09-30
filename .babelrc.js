91687db6330ae7430a5e5676f6aa0f7f64a08012
tophf <tophf@gmx.com>
2019-09-30 17:10:22 +0300

    chore: compile to native ES2015

--- .babelrc.js
+++ .babelrc.js
@@ -5,6 +5,29 @@ module.exports = {
       ...process.env.BABEL_ENV !== 'test' && {
         modules: false,
       },
+      // Allow native implementation of these features (the list is incomplete)
+      exclude: [
+        // Chrome Firefox
+        // 45 22
+        'transform-arrow-functions',
+        // 47 34
+        'transform-computed-properties',
+        // 49 41
+        // 60 55 '...rest' in objects handled by transform-object-rest-spread
+        'transform-destructuring',
+        // 38 13
+        'transform-for-of',
+        // 55 52 - async/await
+        'transform-regenerator',
+        // 47 34
+        'transform-shorthand-properties',
+        // 49 44
+        'transform-sticky-regex',
+        // 41 34
+        'transform-template-literals',
+        // 50 46
+        'transform-unicode-regex',
+      ],
       useBuiltIns: false,
     }],
   ],
