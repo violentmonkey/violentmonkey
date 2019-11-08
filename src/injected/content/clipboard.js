violentmonkey/violentmonkey

commit f720d3e2f983d9b7fae7dbc06e2694ce88b9fb9e

use babel-plugin-transform-function-bind

src/injected/content/clipboard.js
@@ -1,19 +1,26 @@
+import { addEventListener, warn } from '../utils/helpers';
+
+const { execCommand } = Document.prototype;
+const { setData } = DataTransfer.prototype;
+const getClipboardData = Object.getOwnPropertyDescriptor(ClipboardEvent.prototype, 'clipboardData').get;
+const { preventDefault, removeEventListener, stopImmediatePropagation } = EventTarget.prototype;
+
 let clipboardData;
 
 function onCopy(e) {
-  e.stopImmediatePropagation();
-  e.preventDefault();
+  e::stopImmediatePropagation();
+  e::preventDefault();
   const { type, data } = clipboardData;
-  e.clipboardData.setData(type || 'text/plain', data);
+  e::getClipboardData::setData(type || 'text/plain', data);
 }
 
 export default function setClipboard({ type, data }) {
   clipboardData = { type, data };
-  document.addEventListener('copy', onCopy, false);
-  const ret = document.execCommand('copy', false, null);
-  document.removeEventListener('copy', onCopy, false);
+  document::addEventListener('copy', onCopy, false);
+  const ret = document::execCommand('copy', false, null);
+  document::removeEventListener('copy', onCopy, false);
   clipboardData = null;
   if (process.env.DEBUG && !ret) {
-    console.warn('Copy failed!');
+    warn('Copy failed!');
   }
 }

.babelrc.js
@@ -14,4 +14,7 @@ module.exports = {
       useBuiltIns: false,
     }],
   ],
+  plugins: [
+    'transform-function-bind',
+  ],
 };

src/injected/content/index.js
@@ -1,9 +1,11 @@
 import { isFirefox } from '#/common/ua';
 import { getUniqId } from '#/common';
 import { INJECT_PAGE, INJECT_CONTENT, INJECT_AUTO } from '#/common/consts';
+import { bindEvents, sendMessage, attachFunction } from '../utils';
 import {
-  bindEvents, sendMessage, attachFunction, setJsonDump, jsonDump,
-} from '../utils';
+  setJsonDump, jsonDump, objectKeys, filter, forEach, includes, join,
+  append, createElement, setAttribute,
+} from '../utils/helpers';
 import bridge from './bridge';
 import { tabOpen, tabClose, tabClosed } from './tabs';
 import { onNotificationCreate, onNotificationClick, onNotificationClose } from './notifications';
@@ -37,10 +39,13 @@ function setBadge() {
   }));
 }
 
+// Make sure to call obj::method() in code that may run after INJECT_CONTENT userscripts
+const { split } = String.prototype;
+
 const bgHandlers = {
   Command(data) {
-    const id = +data.split(':', 1)[0];
-    const realm = invokableIds.includes(id) && INJECT_CONTENT;
+    const id = +data::split(':', 1)[0];
+    const realm = invokableIds::includes(id) && INJECT_CONTENT;
     bridge.post({ cmd: 'Command', data, realm });
   },
   GetPopup: getPopup,
@@ -51,14 +56,14 @@ const bgHandlers = {
       { data: {}, present: false },
       { data: {}, present: false, realm: INJECT_CONTENT },
     ];
-    Object.keys(data).forEach((id) => {
-      const r = realms[invokableIds.includes(id) ? 1 : 0];
+    objectKeys(data)::forEach((id) => {
+      const r = realms[invokableIds::includes(id) ? 1 : 0];
       r.data[id] = data[id];
       r.present = true;
     });
     realms
-    .filter(r => r.present)
-    .forEach(({ data: d, realm }) => {
+    ::filter(r => r.present)
+    ::forEach(({ data: d, realm }) => {
       bridge.post({ cmd: 'UpdatedValues', data: d, realm });
     });
   },
@@ -201,7 +206,7 @@ const handlers = {
   HttpRequest: httpRequest,
   AbortRequest: abortRequest,
   Inject: injectScript,
-  InjectMulti: data => data.forEach(injectScript),
+  InjectMulti: data => data::forEach(injectScript),
   TabOpen: tabOpen,
   TabClose: tabClose,
   UpdateValue(data) {
@@ -231,12 +236,12 @@ const handlers = {
   },
   AddStyle({ css, callbackId }, realm) {
     const styleId = getUniqId('VMst');
-    const style = document.createElement('style');
-    style.id = styleId;
-    style.textContent = css;
+    const style = document::createElement('style');
+    style::setAttribute('id', styleId);
+    style::append(css);
     // DOM spec allows any elements under documentElement
     // https://dom.spec.whatwg.org/#node-trees
-    (document.head || document.documentElement).appendChild(style);
+    (document.head || document.documentElement)::append(style);
     bridge.post({ cmd: 'Callback', data: { callbackId, payload: styleId }, realm });
   },
   Notification: onNotificationCreate,
@@ -294,7 +299,7 @@ function injectScript(data) {
   if (mode === INJECT_CONTENT) {
     sendMessage({
       cmd: 'InjectScript',
-      data: injectedCode.join(''),
+      data: injectedCode::join(''),
     });
   } else {
     inject(injectedCode, browser.extension.getURL(`/options/index.html#scripts/${scriptId}`));

src/injected/content/util.js
@@ -1,11 +1,15 @@
+import { append, createElement } from '../utils/helpers';
+
+const { remove } = Element.prototype;
+
 export function inject(code, sourceUrl) {
-  const script = document.createElement('script');
+  const script = document::createElement('script');
   // avoid string concatenation of |code| as it can be extremely long
-  script.append(
+  script::append(
     'document.currentScript.remove();',
     ...typeof code === 'string' ? [code] : code,
     ...sourceUrl ? ['\n//# sourceURL=', sourceUrl] : [],
   );
-  document.documentElement.appendChild(script);
-  script.remove();
+  document.documentElement::append(script);
+  script::remove();
 }

src/injected/index.js
@@ -1,4 +1,5 @@
 import { getUniqId, sendMessage } from './utils';
+import { addEventListener, match } from './utils/helpers';
 import initialize from './content';
 
 (function main() {
@@ -16,10 +17,13 @@ import initialize from './content';
 
   initBridge();
 
+  const { go } = History.prototype;
+  const { querySelector } = Document.prototype;
+  const { get: readyState } = Object.getOwnPropertyDescriptor(Document.prototype, 'readyState');
   // For installation
   // Firefox does not support `onBeforeRequest` for `file:`
   function checkJS() {
-    if (!document.querySelector('title')) {
+    if (!document::querySelector('title')) {
       // plain text
       sendMessage({
         cmd: 'ConfirmInstall',
@@ -30,13 +34,13 @@ import initialize from './content';
         },
       })
       .then(() => {
-        if (window.history.length > 1) window.history.go(-1);
+        if (window.history.length > 1) window.history::go(-1);
         else sendMessage({ cmd: 'TabClose' });
       });
     }
   }
-  if (/\.user\.js$/.test(window.location.pathname)) {
-    if (document.readyState === 'complete') checkJS();
-    else window.addEventListener('load', checkJS, false);
+  if (window.location.pathname::match(/\.user\.js$/)) {
+    if (document::readyState() === 'complete') checkJS();
+    else window::addEventListener('load', checkJS, { once: true });
   }
 }());

src/injected/utils/helpers.js
@@ -2,123 +2,36 @@
 // Firefox sucks: `isFinite` is not defined on `window`, see violentmonkey/violentmonkey#300
 // eslint-disable-next-line no-restricted-properties
 export const {
-  console, Promise, isFinite, Uint8Array, setTimeout, atob, Blob, Error, Boolean,
+  // types
+  Blob, Boolean, Error, Promise, Uint8Array,
+  // props and methods
+  atob, console, isFinite, setTimeout,
 } = global;
 
-const arrayProto = Array.prototype;
-const objectProto = Object.prototype;
-const stringProto = String.prototype;
-
-// binding takes times so we need to keep the amount of bound methods low
-// TODO: investigate Babel macros/plugins API to make using safe functions much more convenient
-const bindThis = func => (thisObj, ...args) => func.apply(thisObj, args);
-
-export const concat = bindThis(arrayProto.concat);
-export const filter = bindThis(arrayProto.filter);
-export const forEach = bindThis(arrayProto.forEach);
-export const join = bindThis(arrayProto.join);
-export const map = bindThis(arrayProto.map);
-export const indexOf = bindThis(arrayProto.indexOf);
-export const push = bindThis(arrayProto.push);
-export const shift = bindThis(arrayProto.shift);
-
-export const includes = arrayProto.includes
-  ? bindThis(arrayProto.includes)
-  : (arr, item) => indexOf(arr, item) >= 0;
-
-export const toString = bindThis(objectProto.toString);
-export const stringCharCodeAt = bindThis(stringProto.charCodeAt);
-export const stringLastIndexOf = bindThis(stringProto.lastIndexOf);
-export const stringMatch = bindThis(stringProto.match);
-export const stringSlice = bindThis(stringProto.slice);
-export const stringStarts = bindThis(stringProto.startsWith);
-const numberToString = bindThis(Number.prototype.toString);
-const stringReplace = bindThis(stringProto.replace);
-const stringToLower = bindThis(stringProto.toLowerCase);
-const { fromCharCode } = String;
+export const {
+  filter, forEach, indexOf, join, map, push,
+  // arraySlice, // to differentiate from String::slice which we use much more often
+  includes = function _(item) { return this::indexOf(item) >= 0; },
+} = Array.prototype;
 
 export const { keys: objectKeys, defineProperty, defineProperties } = Object;
-export const assign = Object.assign || ((obj, ...args) => {
-  forEach(args, (arg) => {
-    if (arg) {
-      forEach(objectKeys(arg), (key) => {
-        obj[key] = arg[key];
-      });
-    }
-  });
-  return obj;
-});
+export const { charCodeAt, match, slice } = String.prototype;
+export const { toString: objectToString } = Object.prototype;
+const { toString: numberToString } = Number.prototype;
+const { replace } = String.prototype;
+export const { fromCharCode } = String;
+export const { warn } = console;
+export const { addEventListener } = EventTarget.prototype;
+export const { append, setAttribute } = Element.prototype;
+export const { createElement } = Document.prototype;
 
 export const isArray = obj => (
   // ES3 way, not reliable if prototype is modified
-  // toString(obj) === '[object Array]'
+  // Object's toString(obj) === '[object Array]'
   // #565 steamcommunity.com has overridden `Array.prototype`
   // support duck typing
   obj && typeof obj.length === 'number' && typeof obj.splice === 'function'
 );
-
-export const { warn } = console;
-
-export function encodeBody(body) {
-  const cls = getType(body);
-  let result;
-  if (cls === 'formdata') {
-    // FormData#keys is supported in Chrome >= 50
-    if (!body.keys) return {};
-    const promises = [];
-    const iterator = body.keys();
-    while (1) { // eslint-disable-line no-constant-condition
-      const item = iterator.next();
-      if (item.done) break;
-      const key = item.value;
-      const promise = Promise.all(map(body.getAll(key), encodeBody))
-      .then(values => ({ key, values }));
-      push(promises, promise);
-    }
-    result = Promise.all(promises)
-    .then((items) => {
-      const res = {};
-      forEach(items, (item) => {
-        res[item.key] = item.values;
-      });
-      return res;
-    })
-    .then(value => ({ cls, value }));
-  } else if (includes(['blob', 'file'], cls)) {
-    result = new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        // In Firefox, Uint8Array cannot be sliced if its data is read by FileReader
-        const array = new Uint8Array(reader.result);
-        let value = '';
-        for (let i = 0; i < array.length; i += 1) {
-          value += fromCharCode(array[i]);
-        }
-        resolve({
-          cls,
-          value,
-          type: body.type,
-          name: body.name,
-          lastModified: body.lastModified,
-        });
-      };
-      reader.readAsArrayBuffer(body);
-    });
-  } else if (body) {
-    result = {
-      cls,
-      value: jsonDump(body),
-    };
-  }
-  return Promise.resolve(result);
-}
-
-function getType(obj) {
-  const type = typeof obj;
-  if (type !== 'object') return type;
-  const typeString = toString(obj); // [object TYPENAME]
-  return stringToLower(stringSlice(typeString, 8, -1));
-}
 
 export function noop() {}
 
@@ -133,17 +46,17 @@ export function utf8decode(utftext) {
   let c2 = 0;
   let c3 = 0;
   while (i < utftext.length) {
-    c1 = stringCharCodeAt(utftext, i);
+    c1 = utftext::charCodeAt(i);
     if (c1 < 128) {
       string += fromCharCode(c1);
       i += 1;
     } else if (c1 > 191 && c1 < 224) {
-      c2 = stringCharCodeAt(utftext, i + 1);
+      c2 = utftext::charCodeAt(i + 1);
       string += fromCharCode(((c1 & 31) << 6) | (c2 & 63));
       i += 2;
     } else {
-      c2 = stringCharCodeAt(utftext, i + 1);
-      c3 = stringCharCodeAt(utftext, i + 2);
+      c2 = utftext::charCodeAt(i + 1);
+      c3 = utftext::charCodeAt(i + 2);
       string += fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
       i += 3;
     }
@@ -163,7 +76,7 @@ const escMap = {
   '\t': '\\t',
 };
 const escRE = /[\\"\u0000-\u001F\u2028\u2029]/g; // eslint-disable-line no-control-regex
-const escFunc = m => escMap[m] || `\\u${stringSlice(numberToString(stringCharCodeAt(m, 0) + 0x10000, 16), 1)}`;
+const escFunc = m => escMap[m] || `\\u${(m::charCodeAt(0) + 0x10000)::numberToString(16)::slice(1)}`;
 export const jsonLoad = JSON.parse;
 let jsonDumpFunction = jsonDumpSafe;
 // When running in the page context we must beware of sites that override Array#toJSON
@@ -183,16 +96,16 @@ function jsonDumpSafe(value) {
   if (type === 'boolean') return `${value}`;
   if (type === 'object') {
     if (isArray(value)) {
-      return `[${join(map(value, jsonDumpSafe), ',')}]`;
+      return `[${value::map(jsonDumpSafe)::join(',')}]`;
     }
-    if (toString(value) === '[object Object]') {
-      const res = map(objectKeys(value), (key) => {
+    if (value::objectToString() === '[object Object]') {
+      const res = objectKeys(value)::map((key) => {
         const v = value[key];
         return v !== undefined && `${jsonDumpSafe(key)}:${jsonDumpSafe(v)}`;
       });
       // JSON.stringify skips undefined in objects i.e. {foo: undefined} produces {}
-      return `{${join(filter(res, Boolean), ',')}}`;
+      return `{${res::filter(Boolean)::join(',')}}`;
     }
   }
-  return `"${stringReplace(value, escRE, escFunc)}"`;
+  return `"${value::replace(escRE, escFunc)}"`;
 }

src/injected/utils/index.js
@@ -1,25 +1,24 @@
 import { getUniqId } from '#/common';
-import { jsonDump, jsonLoad } from './helpers';
+import { jsonDump, jsonLoad, addEventListener } from './helpers';
 
 export { getUniqId, jsonDump };
 export {
   sendMessage, request, throttle, cache2blobUrl,
 } from '#/common';
-export { setJsonDump } from './helpers';
 
 const { CustomEvent } = global;
-const { dispatchEvent, addEventListener } = EventTarget.prototype;
+const { dispatchEvent } = EventTarget.prototype;
 
 export function postData(destId, data, asString) {
   // Firefox issue: data must be stringified to avoid cross-origin problem
   const detail = asString ? jsonDump(data) : data;
   const e = new CustomEvent(destId, { detail });
-  dispatchEvent.call(document, e);
+  document::dispatchEvent(e);
 }
 
 /** @returns {PostDataFunction} */
 export function bindEvents(srcId, destId, handle) {
-  addEventListener.call(document, srcId, ({ detail }) => {
+  document::addEventListener(srcId, ({ detail }) => {
     // we use bridge.post() to send an object with cmd and data, never a literal string,
     // so |detail| being a string means it was sent from another context/realm (Firefox only)
     const data = typeof detail === 'string' ? jsonLoad(detail) : detail;

src/injected/web/index.js
@@ -3,9 +3,9 @@ import {
   getUniqId, bindEvents, attachFunction, cache2blobUrl,
 } from '../utils';
 import {
-  includes, forEach, map, push, join, concat, filter, Boolean, utf8decode, jsonDump, jsonLoad, atob,
-  Promise, console, assign, objectKeys, setTimeout, Error, defineProperty, defineProperties,
-  stringLastIndexOf, stringMatch, stringSlice, stringStarts, noop,
+  includes, forEach, map, push, join, filter, Boolean, utf8decode, jsonDump, jsonLoad, atob,
+  Promise, console, objectKeys, setTimeout, Error, defineProperty, defineProperties,
+  match, slice, noop,
 } from '../utils/helpers';
 import bridge from './bridge';
 import { onRequestCreate, onRequestStart, onRequestCallback } from './requests';
@@ -19,10 +19,12 @@ import { onDownload } from './download';
 
 let state = 0;
 
-// rarely used so we'll do an explicit .call() later to reduce init time now
-const { addEventListener } = EventTarget.prototype;
+// Make sure to call safe::methods() in code that may run after userscripts
+
 const { getElementById } = Document.prototype;
-const readyStateGet = Object.getOwnPropertyDescriptor(Document.prototype, 'readyState').get;
+const { lastIndexOf, startsWith } = String.prototype;
+const { concat } = Array.prototype;
+const { Number } = global;
 
 export default function initialize(
   webId,
@@ -42,7 +44,7 @@ export default function initialize(
     bridge.post = bindEvents(webId, contentId, onHandle);
     bridge.post.asString = isFirefox;
   }
-  addEventListener.call(document, 'DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', () => {
     state = 1;
     // Load scripts after being handled by listeners in web page
     Promise.resolve().then(bridge.load);
@@ -82,7 +84,7 @@ const handlers = {
   HttpRequested: onRequestCallback,
   TabClosed: onTabClosed,
   UpdatedValues(updates) {
-    forEach(objectKeys(updates), (id) => {
+    objectKeys(updates)::forEach((id) => {
       if (store.values[id]) store.values[id] = updates[id];
     });
   },
@@ -113,9 +115,9 @@ function onLoadScripts(data) {
   const idle = [];
   const end = [];
   bridge.version = data.version;
-  if (includes([
+  if ([
     'greasyfork.org',
-  ], window.location.host)) {
+  ].includes(window.location.host)) {
     exposeVM();
   }
   // reset load and checkLoad
@@ -130,15 +132,15 @@ function onLoadScripts(data) {
     'document-end': end,
   };
   if (data.scripts) {
-    forEach(data.scripts, (script) => {
+    data.scripts.forEach((script) => {
       const runAt = script.custom.runAt || script.meta.runAt;
       const list = listMap[runAt] || end;
-      push(list, script);
+      list.push(script);
       store.values[script.props.id] = data.values[script.props.id];
     });
     run(start);
   }
-  if (!state && includes(['interactive', 'complete'], readyStateGet.call(document))) {
+  if (!state && ['interactive', 'complete'].includes(document.readyState)) {
     state = 1;
   }
   if (state) bridge.load();
@@ -146,27 +148,27 @@ function onLoadScripts(data) {
   function buildCode(script) {
     const pathMap = script.custom.pathMap || {};
     const requireKeys = script.meta.require || [];
-    const requires = filter(map(requireKeys, key => data.require[pathMap[key] || key]), Boolean);
+    const requires = requireKeys::map(key => data.require[pathMap[key] || key])::filter(Boolean);
     const code = data.code[script.props.id] || '';
     const { wrapper, thisObj, keys } = wrapGM(script, code, data.cache);
     const id = getUniqId('VMin');
     const fnId = getUniqId('VMfn');
     const codeSlices = [
       `function(${
-        join(keys, ',')
+        keys::join(',')
       }){${
-        join(map(keys, name => `this["${name}"]=${name};`), '')
+        keys::map(name => `this["${name}"]=${name};`)::join('')
       }with(this)((define,module,exports)=>{`,
       // 1. trying to avoid string concatenation of potentially huge code slices
       // 2. adding `;` on a new line in case some required script ends with a line comment
-      ...concat([], ...map(requires, req => [req, '\n;'])),
+      ...[]::concat(...requires::map(req => [req, '\n;'])),
       '(()=>{',
       code,
       // adding a new line in case the code ends with a line comment
       '\n})()})()}',
     ];
     const name = script.custom.name || script.meta.name || script.props.id;
-    const args = map(keys, key => wrapper[key]);
+    const args = keys::map(key => wrapper[key]);
     attachFunction(fnId, () => {
       const func = window[id];
       if (func) runCode(name, func, args, thisObj);
@@ -174,7 +176,7 @@ function onLoadScripts(data) {
     return [id, codeSlices, fnId, bridge.mode, script.props.id];
   }
   function run(list) {
-    bridge.post({ cmd: 'InjectMulti', data: map(list, buildCode) });
+    bridge.post({ cmd: 'InjectMulti', data: list::map(buildCode) });
     list.length = 0;
   }
   async function runIdle() {
@@ -197,13 +199,13 @@ function wrapGM(script, code, cache) {
   let thisObj = gm;
   if (!grant.length || (grant.length === 1 && grant[0] === 'none')) {
     // @grant none
-    grant.pop();
+    grant.length = 0;
     gm.window = unsafeWindow;
   } else {
     thisObj = getWrapper();
     gm.window = thisObj;
   }
-  if (includes(grant, 'window.close')) {
+  if (grant::includes('window.close')) {
     gm.window.close = () => {
       bridge.post({ cmd: 'TabClose' });
     };
@@ -211,7 +213,7 @@ function wrapGM(script, code, cache) {
   const resources = script.meta.resources || {};
   const gmInfo = {
     uuid: script.props.uuid,
-    scriptMetaStr: stringMatch(code, METABLOCK_RE)[1] || '',
+    scriptMetaStr: code::match(METABLOCK_RE)[1] || '',
     scriptWillUpdate: !!script.config.shouldUpdate,
     scriptHandler: 'Violentmonkey',
     version: bridge.version,
@@ -223,7 +225,7 @@ function wrapGM(script, code, cache) {
       matches: [...script.meta.match],
       name: script.meta.name || '',
       namespace: script.meta.namespace || '',
-      resources: map(objectKeys(resources), name => ({
+      resources: objectKeys(resources)::map(name => ({
         name,
         url: resources[name],
       })),
@@ -238,11 +240,11 @@ function wrapGM(script, code, cache) {
   };
   let selfData;
   if (!gmApi) gmApi = createGmApiProps();
-  forEach(grant, (name) => {
+  grant::forEach((name) => {
     let prop = gmApi.boundProps[name];
     if (prop) {
       const gmFunction = prop.value;
-      prop = assign({}, prop);
+      prop = { ...prop };
       prop.value = (...args) => gmFunction.apply(selfData, args);
       selfData = true;
     } else {
@@ -289,7 +291,7 @@ function createGmApiProps() {
       if (raw) {
         const type = raw[0];
         const handle = dataDecoders[type];
-        let val = stringSlice(raw, 1);
+        let val = raw::slice(1);
         try {
           if (handle) val = handle(val);
         } catch (e) {
@@ -314,8 +316,8 @@ function createGmApiProps() {
         const key = this.resources[name];
         const raw = this.cache[this.pathMap[key] || key];
         if (!raw) return;
-        const i = stringLastIndexOf(raw, ',');
-        const lastPart = i < 0 ? raw : stringSlice(raw, i + 1);
+        const i = raw::lastIndexOf(',');
+        const lastPart = i < 0 ? raw : raw::slice(i + 1);
         return utf8decode(atob(lastPart));
       }
     },
@@ -356,7 +358,7 @@ function createGmApiProps() {
     GM_addStyle(css) {
       let el = false;
       const callbackId = registerCallback((styleId) => {
-        el = getElementById.call(document, styleId);
+        el = document::getElementById(styleId);
       });
       bridge.post({ cmd: 'AddStyle', data: { css, callbackId } });
       // Mock a Promise without the need for polyfill
@@ -395,8 +397,8 @@ function createGmApiProps() {
     },
   };
   // convert to object property descriptors
-  forEach([props, boundProps], target => {
-    forEach(objectKeys(target), k => {
+  [props, boundProps].forEach(target => {
+    objectKeys(target).forEach(k => {
       target[k] = propertyFromValue(target[k]);
     });
   });
@@ -433,7 +435,7 @@ function propertyToString() {
 function createWrapperMethods(info) {
   const { unsafeWindow } = info;
   const methods = {};
-  forEach([
+  [
     // 'uneval',
     'isFinite',
     'isNaN',
@@ -480,7 +482,7 @@ function createWrapperMethods(info) {
     'setInterval',
     'setTimeout',
     'stop',
-  ], (name) => {
+  ]::forEach((name) => {
     const method = unsafeWindow[name];
     if (method) {
       methods[name] = (...args) => method.apply(unsafeWindow, args);
@@ -506,7 +508,7 @@ function getWrapper() {
     ...info.methods,
   };
   if (!info.propsToWrap) {
-    info.propsToWrap = filter(bridge.props, p => !(p in wrapper));
+    info.propsToWrap = bridge.props::filter(p => !(p in wrapper));
     bridge.props = null;
   }
   function defineProtectedProperty(name) {
@@ -538,8 +540,8 @@ function getWrapper() {
   // A major GC may hit here no matter how we define props
   // all at once, in batches of 10, 100, 500, or one by one.
   // TODO: try Proxy API if userscripts wouldn't notice the difference
-  forEach(info.propsToWrap, (name) => {
-    if (stringStarts(name, 'on')) defineReactedProperty(name);
+  info.propsToWrap::forEach((name) => {
+    if (name::startsWith('on')) defineReactedProperty(name);
     else defineProtectedProperty(name);
   });
   return wrapper;
@@ -547,8 +549,8 @@ function getWrapper() {
 
 function log(level, tags, ...args) {
   const tagList = ['Violentmonkey'];
-  if (tags) push(tagList, ...tags);
-  const prefix = join(map(tagList, tag => `[${tag}]`), '');
+  if (tags) tagList::push(...tags);
+  const prefix = tagList::map(tag => `[${tag}]`)::join('');
   console[level](prefix, ...args);
 }
 

commit 1364fa1391639278310b9feb7b2af45af83c16e6

fix: GM_setClipboard in FF (broken in f720d3e2)

src/injected/content/clipboard.js
@@ -3,10 +3,12 @@ import { sendCmd } from '../utils';
 import { addEventListener, logging } from '../utils/helpers';
 import bridge from './bridge';
 
-const { execCommand } = Document.prototype;
+// old Firefox defines it on a different prototype so we'll just grab it from document directly
+const { execCommand } = document;
 const { setData } = DataTransfer.prototype;
 const getClipboardData = Object.getOwnPropertyDescriptor(ClipboardEvent.prototype, 'clipboardData').get;
-const { preventDefault, removeEventListener, stopImmediatePropagation } = EventTarget.prototype;
+const { preventDefault, stopImmediatePropagation } = Event.prototype;
+const { removeEventListener } = EventTarget.prototype;
 
 let clipboardData;
 
@@ -27,7 +29,7 @@ function onCopy(e) {
   e::stopImmediatePropagation();
   e::preventDefault();
   const { type, data } = clipboardData;
-  e::getClipboardData::setData(type || 'text/plain', data);
+  e::getClipboardData()::setData(type || 'text/plain', data);
 }
 
 function setClipboard({ type, data }) {