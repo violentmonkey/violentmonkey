fa5299d934c3e8063db59909ac1b631028ab3872
tophf <tophf@gmx.com>
2019-12-08 13:56:22 +0300

    fix: don't rely on navigator.userAgent

--- src/common/ua.js
+++ src/common/ua.js
@@ -1,5 +1,14 @@
-const { userAgent } = navigator;
+/* global browser */
 
-export const isFirefox = +userAgent.match(/firefox\/(\d+)|$/i)[1] || false;
-export const isChrome = /chrome\//i.test(userAgent);
-export const isAndroid = /android /i.test(userAgent);
+// UA can be overriden by about:config in FF or devtools in Chrome
+// so we'll test for window.chrome.app which is only defined in Chrome
+// and for browser.runtime.BrowserInfo in Firefox 51+
+export const isChrome = !!window.chrome?.app;
+
+// eslint-disable-next-line import/no-mutable-exports
+export let isFirefox = !isChrome && 'BrowserInfo' in browser.runtime;
+// getBrowserInfo doesn't work in content scripts so if they'll ever need the exact version,
+// we'll have to pass isFirefox in GetInjected data
+browser.runtime.getBrowserInfo?.().then((info) => {
+  isFirefox = info.name === 'Firefox' && parseFloat(info.version);
+});
