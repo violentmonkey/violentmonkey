ed4f002fc520969cd87825f1cfa5aa908489cc45
Gerald <gera2ld@163.com>
2019-12-11 11:08:36 +0800

    chore: `gulp i18n` generates .json files

--- gulpfile.js
+++ gulpfile.js
@@ -139,5 +139,5 @@ const pack = gulp.parallel(manifest, copyFiles, copyI18n);
 exports.clean = clean;
 exports.dev = gulp.series(gulp.parallel(pack, jsDev), watch);
 exports.build = gulp.series(clean, gulp.parallel(pack, jsProd));
-exports.i18n = updateI18n;
+exports.i18n = gulp.parallel(updateI18n, copyI18n);
 exports.check = checkI18n;
