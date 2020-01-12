9bfe121e29b9fe7ac81cc87deba2f2e8dbaf8346
tophf <tophf@gmx.com>
2020-01-12 21:16:19 +0300

    feat: use the faster cloneInto instead of JSON.stringify

--- src/injected/utils/index.js
+++ src/injected/utils/index.js
@@ -1,32 +1,19 @@
-import { getUniqId } from '#/common';
-import { jsonDump, jsonLoad, addEventListener } from './helpers';
+import { addEventListener } from './helpers';
 
-export { getUniqId, jsonDump };
-export {
-  sendCmd, sendMessage, request, throttle, cache2blobUrl,
-} from '#/common';
+export { getUniqId } from '#/common';
+export { sendCmd, sendMessage, cache2blobUrl } from '#/common';
 
 const { CustomEvent } = global;
 const { dispatchEvent } = EventTarget.prototype;
 
-export function postData(destId, data, asString) {
-  // Firefox issue: data must be stringified to avoid cross-origin problem
-  const detail = asString ? jsonDump(data) : data;
-  const e = new CustomEvent(destId, { detail });
-  document::dispatchEvent(e);
-}
-
-/** @returns {PostDataFunction} */
-export function bindEvents(srcId, destId, handle) {
-  document::addEventListener(srcId, ({ detail }) => {
-    // we use bridge.post() to send an object with cmd and data, never a literal string,
-    // so |detail| being a string means it was sent from another context/realm (Firefox only)
-    const data = typeof detail === 'string' ? jsonLoad(detail) : detail;
-    handle(data);
-  });
-  // let post.asString be configurable by the calling code
-  const post = data => postData(destId, data, post.asString);
-  return post;
+export function bindEvents(srcId, destId, handle, cloneInto) {
+  document::addEventListener(srcId, e => handle(e.detail));
+  const pageContext = cloneInto && document.defaultView;
+  return data => {
+    const detail = cloneInto ? cloneInto(data, pageContext) : data;
+    const e = new CustomEvent(destId, { detail });
+    document::dispatchEvent(e);
+  };
 }
 
 // it's injected as a string so only the page functions can be used
@@ -39,11 +26,3 @@ export function attachFunction(id, cb) {
     configurable: true,
   });
 }
-
-/**
- * @typedef {Function} PostDataFunction
- * @property {boolean} asString -
- *   true = stringify data sent via CustomEvent,
- *   required in Firefox when sending between realms
- *   (page context vs content script context)
- */
