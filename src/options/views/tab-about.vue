1688903f680287d0030dc4d507c02abc91fc45e4
Gerald <gera2ld@163.com>
2020-01-29 21:43:12 +0800

    fix: add link to contributors

--- src/options/views/tab-about.vue
+++ src/options/views/tab-about.vue
@@ -5,18 +5,16 @@
       <small v-text="`v${version}`"></small>
     </h1>
     <p class="mb-2" v-text="i18n('extDescription')"></p>
+    <div class="mb-2">
+      <a href="https://violentmonkey.github.io/donate/" target="_blank" rel="noopener noreferrer" v-text="i18n('labelDonate')"></a><span class="text-red"> &hearts;</span>
+    </div>
     <div class="mb-1">
       <label v-text="i18n('labelRelated')"></label>
-      <a href="https://violentmonkey.github.io" target="_blank" rel="noopener noreferrer" v-text="i18n('labelHomepage')"></a> |
-      <a href="https://github.com/violentmonkey/violentmonkey/issues" target="_blank" rel="noopener noreferrer" v-text="i18n('labelFeedback')"></a>
-    </div>
-    <div class="mb-1">
-      <label v-text="i18n('labelAuthor')"></label>
-      <span v-html="i18n('anchorAuthor')"></span> | <a href="https://violentmonkey.github.io/donate/" target="_blank" rel="noopener noreferrer" v-text="i18n('labelDonate')"></a><span class="text-red"> &hearts;</span>
-    </div>
-    <div class="mb-1">
-      <label v-text="i18n('labelTranslator')"></label>
-      <span v-html="i18n('anchorTranslator')"></span>
+      <ul>
+        <li><a href="https://violentmonkey.github.io" target="_blank" rel="noopener noreferrer" v-text="i18n('labelHomepage')"></a></li>
+        <li><a href="https://github.com/violentmonkey/violentmonkey/issues" target="_blank" rel="noopener noreferrer" v-text="i18n('labelFeedback')"></a></li>
+        <li><a href="https://github.com/violentmonkey/violentmonkey/graphs/contributors" target="_blank" rel="noopener noreferrer" v-text="i18n('labelContributors')"></a></li>
+      </ul>
     </div>
     <div class="mb-1">
       <label v-text="i18n('labelCurrentLang')"></label>
