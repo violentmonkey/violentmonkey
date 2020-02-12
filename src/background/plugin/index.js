93e6ecbdc2a9551303e126c7e2146a2aec08ef84
tophf <tophf@gmx.com>
2020-02-12 13:28:08 +0300

    fix: plugin.list() should return a Promise

--- src/background/plugin/index.js
+++ src/background/plugin/index.js
@@ -2,15 +2,27 @@ import { commands } from '../utils/message';
 import { getScripts } from '../utils/db';
 
 export const script = {
-  // Update an existing script identified by the provided id
-  /** @param {{ id, code, message, isNew, config, custom, props, update }} data */
+  /**
+   * Update an existing script identified by the provided id
+   * @param {{ id, code, message, isNew, config, custom, props, update }} data
+   * @return {Promise<{ isNew?, update, where }>}
+   */
   update: commands.ParseScript,
-  // List all available scripts, without script code
-  list: getScripts,
-  // Get script code of an existing script
-  /** @param {number} id */
+  /**
+   * List all available scripts, without script code
+   * @return {Promise<VMScript[]>}
+   */
+  list: async () => getScripts(),
+  /**
+   * Get script code of an existing script
+   * @param {number} id
+   * @return {Promise<string>}
+   */
   get: commands.GetScriptCode,
-  // Remove script
-  /** @param {number} id */
+  /**
+   * Remove script
+   * @param {number} id
+   * @return {Promise<void>}
+   */
   remove: id => commands.MarkRemoved({ id, removed: true }),
 };
