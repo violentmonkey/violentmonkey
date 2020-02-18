f0219e4c0c4c6360d368a0a7ade0f71978b07f39
tophf <tophf@gmx.com>
2020-02-18 18:23:36 +0300

    fix: `chrome` is cleared by injected/web

--- src/common/browser.js
+++ src/common/browser.js
@@ -141,7 +141,8 @@ if (!global.browser?.runtime?.sendMessage) {
 // prefetch the options while the current extension page loads
 /* global browser */
 if (browser.tabs) {
+  const isChrome = !!global.chrome?.app; // saving it now before injected/web clears it
   global.allOptions = browser.runtime.sendMessage({ cmd: 'GetAllOptions' })
-  .then(res => (global.chrome.app ? res.data : res)) // in Chrome we wrap `data` and `error`
+  .then(res => (isChrome ? res.data : res)) // in Chrome we wrap `data` and `error`
   .catch(() => {});
 }
