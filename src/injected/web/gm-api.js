violentmonkey/violentmonkey

commit 01a37a7db6074bb150f599c14d328f076ec65377

fix: sandbox polishing
* improved/fixed the Proxy

* globalKeys is used again to prevent userscripts from seeing page vars directly

* various speedups and fixes

src/injected/web/gm-api.js
@@ -1,7 +1,6 @@
-import { cache2blobUrl, dumpScriptValue, getUniqId, isEmpty } from '#/common';
-import { downloadBlob } from '#/common/download';
+import { dumpScriptValue, getUniqId, isEmpty } from '#/common/util';
 import {
-  defineProperty, objectEntries, objectKeys, objectPick, objectValues,
+  assign, defineProperty, describeProperty, objectEntries, objectKeys, objectPick, objectValues,
 } from '#/common/object';
 import bridge from './bridge';
 import store from './store';
@@ -12,12 +11,21 @@ import {
   decodeValue, dumpValue, loadValues, changeHooks,
 } from './gm-values';
 import {
-  atob, findIndex, indexOf, jsonDump, logging, slice, utf8decode, Error,
+  charCodeAt, jsonDump, logging, slice,
+  append, createElementNS, remove, setAttribute, NS_HTML,
 } from '../utils/helpers';
 
-const { getElementById } = Document.prototype;
-const { lastIndexOf } = String.prototype;
-
+const {
+  atob, setTimeout,
+  Blob, Error, TextDecoder, Uint8Array,
+  Array: { prototype: { findIndex, indexOf } },
+  Document: { prototype: { getElementById } },
+  HTMLElement: { prototype: { click } },
+  String: { prototype: { lastIndexOf } },
+  TextDecoder: { prototype: { decode: tdDecode } },
+  URL: { createObjectURL, revokeObjectURL },
+} = global;
+const { get: getDocElem } = describeProperty(Document.prototype, 'documentElement');
 const vmOwnFuncToString = () => '[Violentmonkey property]';
 export const vmOwnFunc = (func, toString) => {
   defineProperty(func, 'toString', { value: toString || vmOwnFuncToString });
@@ -90,30 +98,10 @@ export function makeGmApi() {
       if (isEmpty(keyHooks)) delete changeHooks[this.id];
     },
     GM_getResourceText(name) {
-      if (name in this.resources) {
-        const key = this.resources[name];
-        const raw = store.cache[this.pathMap[key] || key];
-        if (!raw) return;
-        const i = raw::lastIndexOf(',');
-        const lastPart = i < 0 ? raw : raw::slice(i + 1);
-        return utf8decode(atob(lastPart));
-      }
+      return getResource(this, name);
     },
     GM_getResourceURL(name) {
-      if (name in this.resources) {
-        const key = this.resources[name];
-        let blobUrl = this.urls[key];
-        if (!blobUrl) {
-          const raw = store.cache[this.pathMap[key] || key];
-          if (raw) {
-            blobUrl = cache2blobUrl(raw);
-            this.urls[key] = blobUrl;
-          } else {
-            blobUrl = key;
-          }
-        }
-        return blobUrl;
-      }
+      return getResource(this, name, true);
     },
     GM_registerMenuCommand(cap, func) {
       const { id } = this;
@@ -129,25 +117,24 @@ export function makeGmApi() {
       bridge.post('UnregisterMenu', [id, cap]);
     },
     GM_download(arg1, name) {
-      const opts = typeof arg1 === 'string' ? { url: arg1, name } : arg1;
-      if (!opts || !opts.url) throw new Error('GM_download: Invalid parameter!');
-      return onRequestCreate({
+      // not using ... as it calls Babel's polyfill that calls unsafe Object.xxx
+      const opts = assign({
         method: 'GET',
         responseType: 'blob',
         overrideMimeType: 'application/octet-stream',
-        onload: res => downloadBlob(res.response, opts.name, () => opts.onload?.(res)),
-        ...objectPick(opts, [
-          'url',
-          'headers',
-          'timeout',
-          'onerror',
-          'onprogress',
-          'ontimeout',
-        ]),
-      }, this.id);
+        onload: downloadBlob,
+      }, objectPick(typeof arg1 === 'string' ? { url: arg1, name } : arg1, [
+        'url',
+        'headers',
+        'timeout',
+        'onerror',
+        'onprogress',
+        'ontimeout',
+      ]));
+      opts.context = opts;
+      return onRequestCreate(opts, this.id);
     },
     GM_xmlhttpRequest(opts) {
-      if (!opts || !opts.url) throw new Error('GM_xmlhttpRequest: Invalid parameter!');
       return onRequestCreate(opts, this.id);
     },
     GM_addStyle(css) {
@@ -204,4 +191,49 @@ export function makeGmApi() {
     setClipboard: true,
     addStyle: true, // gm4-polyfill.js sets it anyway
   }];
+}
+
+function getResource(context, name, isBlob) {
+  const key = context.resources[name];
+  if (key) {
+    let res = isBlob && context.urls[key];
+    if (!res) {
+      const raw = store.cache[context.pathMap[key] || key];
+      if (raw) {
+        const dataPos = raw::lastIndexOf(',');
+        const bin = atob(dataPos < 0 ? raw : raw::slice(dataPos + 1));
+        const len = bin.length;
+        const bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i += 1) {
+          bytes[i] = bin::charCodeAt(i);
+        }
+        if (isBlob) {
+          const type = dataPos < 0 ? '' : raw::slice(0, dataPos);
+          res = createObjectURL(new Blob([bytes], { type }));
+          context.urls[key] = res;
+        } else {
+          res = new TextDecoder()::tdDecode(bytes);
+        }
+      } else if (isBlob) {
+        res = key;
+      }
+    }
+    return res;
+  }
+}
+
+function downloadBlob(res) {
+  const { context: { name, onload }, response } = res;
+  const url = createObjectURL(response);
+  const a = document::createElementNS(NS_HTML, 'a');
+  a::setAttribute('hidden', '');
+  a::setAttribute('href', url);
+  if (name) a::setAttribute('download', name);
+  document::getDocElem()::append(a);
+  a::click();
+  setTimeout(() => {
+    a::remove(a);
+    revokeObjectURL(url);
+    onload?.(res);
+  }, 3000);
 }

scripts/sandbox-globals.html
@@ -1,0 +1,52 @@
+<p>Run it both in Chrome and FF</p>
+<textarea id="results" style="width:30em; height: 90%" spellcheck="false"></textarea>
+
+<script>
+const el = document.getElementById('results');
+el.value = [
+  function boundMethods() {
+    const keys = [];
+    /* global globalThis */
+    for (const k in globalThis) {
+      if (k >= 'a' && k <= 'z') {
+        const { value } = Object.getOwnPropertyDescriptor(globalThis, k)
+        || Object.getOwnPropertyDescriptor(EventTarget.prototype, k)
+        || Object.getOwnPropertyDescriptor(Object.prototype, k)
+        || {};
+        if (typeof value === 'function') {
+          if (k === 'createImageBitmap' || k === 'fetch') {
+            keys.push(k);
+          } else {
+            try {
+              globalThis[k].call({});
+            } catch (e) {
+              if (/Illegal invocation|implement interface/.test(e)) {
+                keys.push(k);
+              }
+            }
+          }
+        }
+      }
+    }
+    return keys.sort().map(k => `'${k}',`).join('\n');
+  },
+  function unforgeables() {
+    return Object.entries(Object.getOwnPropertyDescriptors(window))
+    .filter(([, v]) => !v.configurable)
+    .map(([k]) => `'${k}',`)
+    .sort()
+    .join('\n');
+  },
+  function readonlys() {
+    return Object.entries(Object.getOwnPropertyDescriptors(window))
+    .filter(([k, v]) => k >= 'a' && k <= 'z'
+      && k !== 'webkitStorageInfo' // deprecated
+      && v.get && !v.set && v.configurable)
+    .map(([k]) => `'${k}',`)
+    .sort()
+    .join('\n');
+  },
+].map(fn => `// ${fn.name}\n${fn()}\n`).join('\n');
+el.focus();
+el.select();
+</script>

scripts/webpack.conf.js
@@ -67,7 +67,7 @@ const modify = (extra, init) => modifyWebpackConfig(
 
 // avoid running webpack bootstrap in a potentially hacked environment
 // after documentElement was replaced which triggered reinjection of content scripts
-const skipReinjectionHeader = `if (window[Symbol.for('${INIT_FUNC_NAME}')] !== 1)`;
+const skipReinjectionHeader = `if (window['${INIT_FUNC_NAME}'] !== 1)`;
 const skipReinjectionConfig = (config, test) => config.plugins.push(
   new WrapperWebpackPlugin({
     header: skipReinjectionHeader,
@@ -94,7 +94,7 @@ module.exports = Promise.all([
     config.plugins.push(
       new WrapperWebpackPlugin({
         header: `${skipReinjectionHeader}
-          window[Symbol.for('${INIT_FUNC_NAME}')] = function () {
+          window['${INIT_FUNC_NAME}'] = function () {
             var module = { exports: {} };
           `,
         footer: `

src/injected/content/index.js
@@ -36,7 +36,7 @@ const { split } = String.prototype;
   if (data.scripts) injectScripts(contentId, webId, data, isXml);
   isPopupShown = data.isPopupShown;
   sendSetPopup();
-})();
+})().catch(!global.chrome.app && console.error); // Firefox can't show exceptions in content scripts
 
 bridge.addBackgroundHandlers({
   Command(data) {

src/injected/content/inject.js
@@ -9,22 +9,22 @@ import { sendCmd } from '#/common';
 import { defineProperty, describeProperty, forEachEntry, objectPick } from '#/common/object';
 
 import {
-  forEach, push, setTimeout,
-  append, createElementNS, remove, DocProto, NS_HTML,
+  forEach, push,
+  append, createElementNS, remove, NS_HTML,
 } from '../utils/helpers';
 import bridge from './bridge';
 
 // Firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1408996
-const VMInitInjection = window[Symbol.for(process.env.INIT_FUNC_NAME)];
+const VMInitInjection = window[process.env.INIT_FUNC_NAME];
 // To avoid running repeatedly due to new `document.documentElement`
-// (the symbol is undeletable so a userscript can't fool us on reinjection)
-defineProperty(window, Symbol.for(process.env.INIT_FUNC_NAME), { value: 1 });
+// (the prop is undeletable so a userscript can't fool us on reinjection)
+defineProperty(window, process.env.INIT_FUNC_NAME, { value: 1 });
 
-const { document } = global;
+const { document, setTimeout } = global;
 // Userscripts in content mode may redefine head and documentElement
-const { get: getHead } = describeProperty(DocProto, 'head');
-const { get: getDocElem } = describeProperty(DocProto, 'documentElement');
-const { appendChild } = DocProto; // same as Node.appendChild
+const { get: getHead } = describeProperty(Document.prototype, 'head');
+const { get: getDocElem } = describeProperty(Document.prototype, 'documentElement');
+const { appendChild } = Document.prototype; // same as Node.appendChild
 
 export function appendToRoot(node) {
   // DOM spec allows any elements under documentElement
@@ -62,10 +62,12 @@ export function injectScripts(contentId, webId, data, isXml) {
     },
   };
   const triage = (script) => {
-    const { custom, meta } = script;
+    const { custom, dataKey, meta } = script;
     const desiredRealm = custom.injectInto || meta.injectInto || data.injectInto;
     const internalRealm = INJECT_MAPPING[desiredRealm] || INJECT_MAPPING[INJECT_AUTO];
     const realm = internalRealm.find(key => realms[key]?.injectable());
+    // If the script wants this specific realm, which is unavailable, we won't inject it at all
+    if (!realm) return [dataKey, 'done'];
     const { ids, lists } = realms[realm];
     let runAt = bornReady ? 'start'
       : `${custom.runAt || meta.runAt || ''}`.replace(/^document-/, '');
@@ -76,7 +78,7 @@ export function injectScripts(contentId, webId, data, isXml) {
     script.action = action;
     ids::push(script.props.id);
     list::push(script);
-    return [script.dataKey, action];
+    return [dataKey, action];
   };
   const feedback = data.scripts.map(triage);
   setupContentInvoker(realms, contentId, webId);

src/injected/utils/helpers.js
@@ -2,69 +2,28 @@
 import { numberToString } from '#/common';
 import { assign, objectKeys } from '#/common/object';
 
-// Firefox sucks: `isFinite` is not defined on `window`, see violentmonkey/violentmonkey#300
-// eslint-disable-next-line no-restricted-properties
-export const {
-  // types
-  Blob, Boolean, Error, Promise, Uint8Array,
-  // props and methods
-  atob, isFinite, setTimeout,
-} = global;
+export const { Promise } = global;
 
-export const {
-  concat, filter, findIndex, forEach, includes, indexOf, join, map, push,
-  // arraySlice, // to differentiate from String::slice which we use much more often
-} = Array.prototype;
+export const { filter, forEach, includes, join, map, push } = Array.prototype;
 export const { charCodeAt, slice, replace } = String.prototype;
 export const { toString: objectToString } = Object.prototype;
-export const { fromCharCode } = String;
 export const { addEventListener, removeEventListener } = EventTarget.prototype;
 export const { append, remove, setAttribute } = Element.prototype;
-export const DocProto = Document.prototype;
-export const { createElementNS } = DocProto;
+export const { createElementNS } = Document.prototype;
 export const logging = assign({}, console);
 
 export const NS_HTML = 'http://www.w3.org/1999/xhtml';
 
-export const isArray = obj => (
+// Firefox defines `isFinite` on `global` not on `window`
+const { Boolean, Uint8Array, isFinite } = global; // eslint-disable-line no-restricted-properties
+const { fromCharCode } = String;
+const isArray = obj => (
   // ES3 way, not reliable if prototype is modified
   // Object.prototype.toString.call(obj) === '[object Array]'
   // #565 steamcommunity.com has overridden `Array.prototype`
   // support duck typing
   obj && typeof obj.length === 'number' && typeof obj.splice === 'function'
 );
-
-export function noop() {}
-
-/**
- * http://www.webtoolkit.info/javascript-utf8.html
- */
-export function utf8decode(utftext) {
-  /* eslint-disable no-bitwise */
-  let string = '';
-  let i = 0;
-  let c1 = 0;
-  let c2 = 0;
-  let c3 = 0;
-  while (i < utftext.length) {
-    c1 = utftext::charCodeAt(i);
-    if (c1 < 128) {
-      string += fromCharCode(c1);
-      i += 1;
-    } else if (c1 > 191 && c1 < 224) {
-      c2 = utftext::charCodeAt(i + 1);
-      string += fromCharCode(((c1 & 31) << 6) | (c2 & 63));
-      i += 2;
-    } else {
-      c2 = utftext::charCodeAt(i + 1);
-      c3 = utftext::charCodeAt(i + 2);
-      string += fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-      i += 3;
-    }
-  }
-  return string;
-  /* eslint-enable no-bitwise */
-}
 
 // Reference: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/JSON#Polyfill
 const escMap = {

src/injected/web/bridge.js
@@ -1,12 +1,12 @@
 import { getUniqId } from '#/common';
 import { assign } from '#/common/object';
-import { noop, Promise } from '../utils/helpers';
+import { Promise } from '../utils/helpers';
 
 const handlers = {};
 const callbacks = {};
 const bridge = {
   callbacks,
-  load: noop,
+  load: () => {},
   addHandlers(obj) {
     assign(handlers, obj);
   },

commit bfd647ab306b3705d047b603d5bcea45f0ab05f9

fix: gm_download broken in 01a37a7d

src/injected/web/gm-api.js
@@ -118,20 +118,29 @@ export function makeGmApi() {
     },
     GM_download(arg1, name) {
       // not using ... as it calls Babel's polyfill that calls unsafe Object.xxx
-      const opts = assign({
+      let opts = {};
+      let onload;
+      if (typeof arg1 === 'string') {
+        opts = { url: arg1, name };
+      } else if (arg1) {
+        name = arg1.name;
+        onload = arg1.onload;
+        opts = objectPick(arg1, [
+          'url',
+          'headers',
+          'timeout',
+          'onerror',
+          'onprogress',
+          'ontimeout',
+        ]);
+      }
+      assign(opts, {
+        context: { name, onload },
         method: 'GET',
         responseType: 'blob',
         overrideMimeType: 'application/octet-stream',
         onload: downloadBlob,
-      }, objectPick(typeof arg1 === 'string' ? { url: arg1, name } : arg1, [
-        'url',
-        'headers',
-        'timeout',
-        'onerror',
-        'onprogress',
-        'ontimeout',
-      ]));
-      opts.context = opts;
+      });
       return onRequestCreate(opts, this.id);
     },
     GM_xmlhttpRequest(opts) {