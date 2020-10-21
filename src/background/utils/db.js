b066965516ca4b980850f7bf5bfcb5815f2944db
tophf <tophf@gmx.com>
2020-10-21 12:31:02 +0300

    fixup: typo

--- src/background/utils/db.js
+++ src/background/utils/db.js
@@ -361,8 +361,8 @@ function getUUID() {
   // xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
   // We're using UUIDv4 variant 1 so N=4 and M=8
   // See format_uuid_v3or5 in https://tools.ietf.org/rfc/rfc4122.txt
-  rnd[3] = rnd[1] & 0x0FFF | 0x4000; // eslint-disable-line no-bitwise
-  rnd[4] = rnd[2] & 0x3FFF | 0x8000; // eslint-disable-line no-bitwise
+  rnd[3] = rnd[3] & 0x0FFF | 0x4000; // eslint-disable-line no-bitwise
+  rnd[4] = rnd[4] & 0x3FFF | 0x8000; // eslint-disable-line no-bitwise
   return '01-2-3-4-567'.replace(/\d/g, i => (rnd[i] + 0x1_0000).toString(16).slice(-4));
 }
 
