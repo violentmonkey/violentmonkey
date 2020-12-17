f2d0d854a1a38f36261d73ca5b3a0c1b901895f1
tophf <tophf@gmx.com>
2020-12-17 14:45:37 +0300

    refactor: simplify downloadUrl()

--- src/common/download.js
+++ src/common/download.js
@@ -1,14 +1,9 @@
 export function downloadUrl(url, name, callback) {
   const a = document.createElement('a');
-  a.style.display = 'none';
-  document.body.appendChild(a);
   a.href = url;
-  if (name) a.download = name;
-  a.click();
-  setTimeout(() => {
-    document.body.removeChild(a);
-    if (callback) callback();
-  }, 3000);
+  a.download = name || '';
+  a.dispatchEvent(new MouseEvent('click'));
+  if (callback) setTimeout(callback, 3000);
 }
 
 export function downloadBlob(blob, name, callback) {
