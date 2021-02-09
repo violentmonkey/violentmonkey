violentmonkey/violentmonkey

commit a11484cfee2fdff529dd359d282d254b2f3ec059

feat: option to open editor in new window from popup

src/background/utils/tabs.js
@@ -1,7 +1,8 @@
-import { getActiveTab, sendTabCmd, getFullUrl } from '#/common';
+import { getActiveTab, noop, sendTabCmd, getFullUrl } from '#/common';
 import ua from '#/common/ua';
 import { extensionRoot } from './init';
 import { commands } from './message';
+import { getOption } from './options';
 
 const openers = {};
 
@@ -12,6 +13,7 @@ Object.assign(commands, {
     active = true,
     container,
     insert = true,
+    maybeInWindow = false,
     pinned,
   }, src = {}) {
     // src.tab may be absent when invoked from popup (e.g. edit/create buttons)
@@ -21,23 +23,40 @@ Object.assign(commands, {
     const isInternal = !srcUrl || srcUrl.startsWith(extensionRoot);
     // only incognito storeId may be specified when opening in an incognito window
     const { incognito, windowId } = srcTab;
+    // Chrome can't open chrome-xxx: URLs in incognito windows
+    const canOpenIncognito = !incognito || ua.isFirefox || !/^(chrome[-\w]*):/.test(url);
     let storeId = srcTab.cookieStoreId;
     if (storeId && !incognito) {
       storeId = getContainerId(isInternal ? 0 : container) || storeId;
     }
+    if (storeId) storeId = { cookieStoreId: storeId };
     if (!url.startsWith('blob:')) {
       // URL needs to be expanded to check the protocol for 'chrome' below
       if (!isInternal) url = getFullUrl(url, srcUrl);
       else if (!/^\w+:/.test(url)) url = browser.runtime.getURL(url);
     }
-    const { id, windowId: newWindowId } = await browser.tabs.create({
+    let newTab;
+    if (maybeInWindow && browser.windows && getOption('editorWindow')) {
+      const wndOpts = {
+        url,
+        incognito: canOpenIncognito && incognito,
+        ...getOption('editorWindowSimple') && { type: 'popup' },
+        ...ua.isChrome && { focused: !!active }, // FF doesn't support this
+        ...storeId,
+      };
+      const pos = getOption('editorWindowPos');
+      const hasPos = pos && 'top' in pos;
+      const wnd = await browser.windows.create({ ...wndOpts, ...pos }).catch(hasPos && noop)
+        || hasPos && await browser.windows.create(wndOpts);
+      newTab = wnd.tabs[0];
+    }
+    const { id, windowId: newWindowId } = newTab || await browser.tabs.create({
       url,
       // normalizing as boolean because the API requires strict types
       active: !!active,
       pinned: !!pinned,
-      ...storeId && { cookieStoreId: storeId },
-      // Chrome can't open chrome-xxx: URLs in incognito windows
-      ...(!incognito || ua.isFirefox || !/^(chrome[-\w]*):/.test(url)) && {
+      ...storeId,
+      ...canOpenIncognito && {
         windowId,
         ...insert && { index: srcTab.index + 1 },
         ...ua.openerTabIdSupported && { openerTabId: srcTab.id },

src/_locales/en/messages.yml
@@ -590,6 +590,15 @@ msgUpdating:
 noValues:
   description: Label shown when there is no value for current script.
   message: No value is stored
+optionEditorWindow:
+  description: Label for the option in settings
+  message: Open editor from popup in a new window
+optionEditorWindowHint:
+  description: Tooltip for optionEditorWindow in case the browser doesn't support onBoundsChanged
+  message: Editor window position will be remembered only on resizing or saving
+optionEditorWindowSimple:
+  description: Label for the editor window type
+  message: Hide omnibox
 optionPopupEnabledFirst:
   description: Option to show enabled scripts first in popup.
   message: Enabled first

src/common/browser.js
@@ -108,6 +108,8 @@ if (!global.browser?.runtime?.sendMessage) {
     },
     webRequest: true,
     windows: {
+      create: wrapAsync,
+      getCurrent: wrapAsync,
       update: wrapAsync,
     },
   };

src/common/options-defaults.js
@@ -46,6 +46,9 @@ export default {
     tabSize: 2,
     undoDepth: 200,
   },
+  editorWindow: false, // whether popup opens editor in a new window
+  editorWindowPos: {}, // { left, top, width, height }
+  editorWindowSimple: true, // whether to open a simplified popup or a normal browser window
   scriptTemplate: `\
 // ==UserScript==
 // @name        New script {{name}}

src/options/views/edit/index.vue
@@ -55,9 +55,10 @@
 
 <script>
 import CodeMirror from 'codemirror';
-import { i18n, sendCmd } from '#/common';
+import { debounce, i18n, sendCmd } from '#/common';
 import { deepCopy, deepEqual, forEachEntry, objectPick } from '#/common/object';
 import VmCode from '#/common/ui/code';
+import options from '#/common/options';
 import { route } from '#/common/router';
 import { store, showConfirmation, showMessage } from '../../utils';
 import VmSettings from './settings';
@@ -89,6 +90,34 @@ const toList = text => (
 );
 let saved;
 let showingConfirmation;
+
+let shouldSavePositionOnSave;
+const savePosition = () => {
+  if (options.get('editorWindow')) {
+    options.set('editorWindowPos', {
+      left: window.screenX,
+      top: window.screenY,
+      width: window.outerWidth,
+      height: window.outerHeight,
+    });
+  }
+};
+/** @param {chrome.windows.Window} _ */
+const setupSavePosition = ({ id: curWndId, tabs }) => {
+  if (tabs.length === 1) {
+    const { onBoundsChanged } = global.chrome.windows;
+    if (onBoundsChanged) {
+      // triggered on moving/resizing, Chrome 86+
+      onBoundsChanged.addListener(wnd => {
+        if (wnd.id === curWndId) savePosition();
+      });
+    } else {
+      // triggered on resizing only
+      window.addEventListener('resize', debounce(savePosition, 100));
+      shouldSavePositionOnSave = true;
+    }
+  }
+};
 
 let K_SAVE; // deduced from the current CodeMirror keymap
 const K_PREV_PANEL = 'Alt-PageUp';
@@ -163,6 +192,9 @@ export default {
   created() {
     this.script = this.initial;
     document.addEventListener('keydown', this.switchPanel);
+    if (options.get('editorWindow') && global.history.length === 1) {
+      browser.windows?.getCurrent({ populate: true }).then(setupSavePosition);
+    }
   },
   async mounted() {
     this.nav = 'code';
@@ -212,6 +244,7 @@ export default {
   },
   methods: {
     async save() {
+      if (shouldSavePositionOnSave) savePosition();
       const { code, settings } = this;
       const { config, custom } = settings;
       const { notifyUpdates } = config;

src/options/views/tab-settings/index.vue
@@ -10,6 +10,18 @@
         </label>
       </div>
       <div class="mb-1">
+        <label class="mr-2" >
+          <setting-check name="editorWindow" />
+          <tooltip :content="editorWindowHint" :disabled="!editorWindowHint">
+            <span v-text="i18n('optionEditorWindow')"></span>
+          </tooltip>
+        </label>
+        <label>
+          <setting-check name="editorWindowSimple" />
+          <span v-text="i18n('optionEditorWindowSimple')"></span>
+        </label>
+      </div>
+      <div class="mb-1">
         <label>
           <locale-group i18n-key="labelPopupSort">
             <select v-model="settings['filtersPopup.sort']">
@@ -103,6 +115,7 @@
 </template>
 
 <script>
+import Tooltip from 'vueleton/lib/tooltip/bundle';
 import { debounce } from '#/common';
 import {
   INJECT_AUTO,
@@ -167,6 +180,7 @@ export default {
     VmCss,
     SettingCheck,
     LocaleGroup,
+    Tooltip,
   },
   data() {
     return {
@@ -175,6 +189,11 @@ export default {
       settings,
       injectIntoOptions,
     };
+  },
+  computed: {
+    editorWindowHint() {
+      return global.chrome.windows?.onBoundsChanged ? null : this.i18n('optionEditorWindowHint');
+    },
   },
   methods: {
     getUpdater({ name, normalize }) {

src/popup/views/app.vue
@@ -239,6 +239,7 @@ export default {
     onEditScript(item) {
       sendCmd('TabOpen', {
         url: `/options/index.html#scripts/${item.data.props.id}`,
+        maybeInWindow: true,
       });
       window.close();
     },
@@ -274,6 +275,7 @@ export default {
       });
       sendCmd('TabOpen', {
         url: `/options/index.html#scripts/_new${id ? `/${id}` : ''}`,
+        maybeInWindow: true,
       });
       window.close();
     },

commit aa622dee1f53da56393a47ebc9180bbb2e441b48

fix: open editor from incognito windows
fixes #1190
regressed in a11484cf

src/background/utils/tabs.js
@@ -30,17 +30,17 @@ Object.assign(commands, {
     // only incognito storeId may be specified when opening in an incognito window
     const { incognito, windowId } = srcTab;
     // Chrome can't open chrome-xxx: URLs in incognito windows
-    const canOpenIncognito = !incognito || ua.isFirefox || !/^(chrome[-\w]*):/.test(url);
     let storeId = srcTab.cookieStoreId;
     if (storeId && !incognito) {
       storeId = getContainerId(isInternal ? 0 : container) || storeId;
     }
     if (storeId) storeId = { cookieStoreId: storeId };
     if (!url.startsWith('blob:')) {
-      // URL needs to be expanded to check the protocol for 'chrome' below
+      // URL needs to be expanded for `canOpenIncognito` below
       if (!isInternal) url = getFullUrl(url, srcUrl);
       else if (!/^\w+:/.test(url)) url = browser.runtime.getURL(url);
     }
+    const canOpenIncognito = !incognito || ua.isFirefox || !/^(chrome[-\w]*):/.test(url);
     let newTab;
     if (maybeInWindow && browser.windows && getOption('editorWindow')) {
       const wndOpts = {