f0d4df246ca08e30abd9d22226c80e56c57f785a
tophf <tophf@gmx.com>
2021-02-28 14:12:48 +0300

    fix: read active tab's URL for newScript hotkey (#1215)

--- src/background/utils/hotkeys.js
+++ src/background/utils/hotkeys.js
@@ -1,15 +1,13 @@
 import { postInitialize } from './init';
 import { commands } from './message';
 
-const ROUTES = {
-  newScript: '#scripts/_new',
-  settings: '#settings',
-};
-
 postInitialize.push(() => {
   browser.commands.onCommand.addListener((cmd) => {
-    commands.TabOpen({
-      url: `${browser.runtime.getManifest().options_ui.page}${ROUTES[cmd] || ''}`,
-    });
+    if (cmd === 'newScript') {
+      commands.OpenEditor();
+    } else {
+      const route = cmd === 'settings' ? `#${cmd}` : '';
+      commands.TabOpen({ url: `/options/index.html${route}` });
+    }
   });
 });
