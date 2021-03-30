3e9fb38db4f77fe8e0c7d8a490b44c4be0a4be3c
tophf <tophf@gmx.com>
2021-03-30 09:48:33 +0300

    fix: increase the default line length limit

--- src/common/ui/code.vue
+++ src/common/ui/code.vue
@@ -97,7 +97,10 @@ export const cmOptions = {
   autoCloseBrackets: true,
   highlightSelectionMatches: true,
   keyMap: 'sublime',
-  maxDisplayLength: 20_000,
+  /* Limiting the max length to avoid delays while CodeMirror tries to make sense of a long line.
+   * 100kB is fast enough for the main editor (moreover such long lines are rare in the main script),
+   * and is big enough to include most of popular minified libraries for the `@resource/@require` viewer. */
+  maxDisplayLength: 100_000,
 };
 
 export default {
