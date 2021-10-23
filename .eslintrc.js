violentmonkey/violentmonkey

commit 2c0a2202b33948f0b5098497bf9ded35bd0fd19a

fix: use no-cache for local urls and updates

src/common/util.js
@@ -257,6 +257,10 @@ export async function requestLocalFile(url, options = {}) {
 const FORCED_ACCEPT = {
   'greasyfork.org': 'application/javascript, text/plain, text/css',
 };
+
+export const isRemote = url => url
+  && !(/^(file:|data:|https?:\/\/(localhost|127\.0\.0\.1[:/]))/.test(url));
+
 /** @typedef {{
   url: string,
   status: number,
@@ -272,13 +276,13 @@ const FORCED_ACCEPT = {
 export async function request(url, options = {}) {
   // fetch does not support local file
   if (url.startsWith('file://')) return requestLocalFile(url, options);
-  const { body, credentials, headers, method, responseType } = options;
+  const { body, headers, responseType } = options;
   const isBodyObj = body && body::({}).toString() === '[object Object]';
   const hostname = url.split('/', 3)[2];
   const accept = FORCED_ACCEPT[hostname];
   const init = {
-    credentials,
-    method,
+    cache: isRemote(url) ? undefined : 'no-cache',
+    ...options, /* Used in safe context */// eslint-disable-line no-restricted-syntax
     body: isBodyObj ? JSON.stringify(body) : body,
     headers: isBodyObj || accept
       ? Object.assign({},

src/background/utils/update.js
@@ -38,9 +38,6 @@ async function checkAllAndNotify(scripts) {
 }
 
 const processes = {};
-const NO_HTTP_CACHE = {
-  'Cache-Control': 'no-cache, no-store, must-revalidate',
-};
 
 // resolves to true if successfully updated
 function checkUpdate(script, notes) {
@@ -61,7 +58,7 @@ async function doCheckUpdate(script, notes) {
       update: { checking: false },
     });
     msgOk = i18n('msgScriptUpdated', [getScriptName(update)]);
-    resourceOpts = { headers: NO_HTTP_CACHE };
+    resourceOpts = { cache: 'no-cache' };
     return true;
   } catch (update) {
     msgErr = update.error;
@@ -93,7 +90,9 @@ async function downloadUpdate({ props: { id }, meta, custom }) {
   announce(i18n('msgCheckingForUpdate'));
   try {
     const { data } = await request(updateURL, {
-      headers: { ...NO_HTTP_CACHE, Accept: 'text/x-userscript-meta,*/*' },
+      // TODO: do a HEAD request first to get ETag header and compare to storage.mod
+      cache: 'no-cache',
+      headers: { Accept: 'text/x-userscript-meta,*/*' },
     });
     const { version } = parseMeta(data);
     if (compareVersion(meta.version, version) >= 0) {
@@ -103,7 +102,7 @@ async function downloadUpdate({ props: { id }, meta, custom }) {
     } else {
       announce(i18n('msgUpdating'));
       errorMessage = i18n('msgErrorFetchingScript');
-      return (await request(downloadURL, { headers: NO_HTTP_CACHE })).data;
+      return (await request(downloadURL, { cache: 'no-cache' })).data;
     }
   } catch (error) {
     if (process.env.DEBUG) console.error(error);

src/common/index.js
@@ -149,10 +149,6 @@ export function getFullUrl(url, base) {
   return obj.href;
 }
 
-export function isRemote(url) {
-  return url && !(/^(file:|data:|https?:\/\/localhost[:/]|http:\/\/127\.0\.0\.1[:/])/.test(url));
-}
-
 export function encodeFilename(name) {
   // `escape` generated URI has % in it
   return name.replace(/[-\\/:*?"<>|%\s]/g, (m) => {

commit efd6a4d01a763235837a27943233aff5b37a8457

fixup: add the missing "?.", remove spread polyfill
regressed in 2c0a2202

.eslintrc.js
@@ -57,7 +57,7 @@ module.exports = {
          Ideally, `eslint-plugin-compat` should be used but I couldn't make it work. */
       'no-restricted-syntax': ['error', {
         selector: 'ObjectExpression > ExperimentalSpreadProperty',
-        message: 'Object spread in an unsafe environment',
+        message: 'Object spread adds a polyfill in injected* even if unused by it',
       }, {
         selector: 'OptionalCallExpression',
         message: 'Optional call in an unsafe environment',

src/background/utils/db.js
@@ -557,7 +557,7 @@ export async function fetchResources(script, resourceCache, reqOptions) {
     ...Object.values(meta.resources).map(url => snatch(url, 'cache')),
     isRemote(meta.icon) && snatch(meta.icon, 'cache', validateImage),
   ]);
-  if (!resourceCache.ignoreDepsErrors) {
+  if (!resourceCache?.ignoreDepsErrors) {
     const error = errors.map(formatHttpError)::trueJoin('\n');
     if (error) {
       const message = i18n('msgErrorFetchingResource');

src/common/util.js
@@ -280,9 +280,10 @@ export async function request(url, options = {}) {
   const isBodyObj = body && body::({}).toString() === '[object Object]';
   const hostname = url.split('/', 3)[2];
   const accept = FORCED_ACCEPT[hostname];
-  const init = {
+  // Not using ...spread because Babel mistakenly adds its polyfill to injected-web
+  const init = Object.assign({
     cache: isRemote(url) ? undefined : 'no-cache',
-    ...options, /* Used in safe context */// eslint-disable-line no-restricted-syntax
+  }, options, {
     body: isBodyObj ? JSON.stringify(body) : body,
     headers: isBodyObj || accept
       ? Object.assign({},
@@ -290,7 +291,7 @@ export async function request(url, options = {}) {
         isBodyObj && { 'Content-Type': 'application/json' },
         accept && { accept })
       : headers,
-  };
+  });
   const result = { url, status: -1 };
   try {
     const resp = await fetch(url, init);