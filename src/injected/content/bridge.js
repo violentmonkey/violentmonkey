violentmonkey/violentmonkey

commit 6dac93292b17262014293ac18f04370bc62eead7

fix: check web->content permissions

src/injected/content/bridge.js
@@ -1,10 +1,18 @@
 import { sendCmd } from '#/common';
 import { INJECT_PAGE, browser } from '#/common/consts';
 
-// {CommandName: sendCmd} will relay the request via sendCmd as is
+const allow = createNullObj();
 /** @type {Object.<string, MessageFromGuestHandler>} */
 const handlers = createNullObj();
 const bgHandlers = createNullObj();
+const onScripts = [];
+const assignHandlers = (dest, src, force) => {
+  if (force) {
+    assign(dest, src);
+  } else {
+    onScripts.push(() => assign(dest, src));
+  }
+};
 const bridge = {
   __proto__: null, // Object.create(null) may be spoofed
   ids: [], // all ids including the disabled ones for SetPopup
@@ -13,21 +21,29 @@ const bridge = {
   /** @type Number[] */
   invokableIds: [],
   failedIds: [],
-  // {CommandName: sendCmd} will relay the request via sendCmd as is
-  addHandlers(obj) {
-    assign(handlers, obj);
+  onScripts,
+  /** Without `force` handlers will be added only when userscripts are about to be injected. */
+  addHandlers(obj, force) {
+    assignHandlers(handlers, obj, force);
   },
-  addBackgroundHandlers(obj) {
-    assign(bgHandlers, obj);
+  /** { CommandName: true } will relay the request via sendCmd as is.
+   * Without `force` handlers will be added only when userscripts are about to be injected. */
+  addBackgroundHandlers(obj, force) {
+    assignHandlers(bgHandlers, obj, force);
+  },
+  allow(cmd, dataKey) {
+    (allow[cmd] || (allow[cmd] = createNullObj()))[dataKey] = true;
   },
   // realm is provided when called directly via invokeHost
-  async onHandle({ cmd, data }, realm) {
+  async onHandle({ cmd, data, dataKey }, realm) {
     const handle = handlers[cmd];
-    if (!handle) throw new Error(`Invalid command: ${cmd}`);
+    if (!handle || !allow[cmd]?.[dataKey]) {
+      throw new Error(`[Violentmonkey] Invalid command: "${cmd}" on ${global.location.host}`);
+    }
     const callbackId = data?.callbackId;
     const payload = callbackId ? data.payload : data;
-    let res = handle === sendCmd ? sendCmd(cmd, payload) : handle(payload, realm || INJECT_PAGE);
-    if (typeof res?.then === 'function') {
+    let res = handle === true ? sendCmd(cmd, payload) : handle(payload, realm || INJECT_PAGE);
+    if (res instanceof Promise) {
       res = await res;
     }
     if (callbackId && res !== undefined) {

src/injected/web/bridge.js
@@ -13,25 +13,25 @@ const bridge = {
     const fn = handlers[cmd];
     if (fn) fn(data);
   },
-  send(cmd, data) {
+  send(cmd, data, context) {
     return new Promise(resolve => {
-      postWithCallback(cmd, data, resolve);
+      postWithCallback(cmd, data, context, resolve);
     });
   },
-  sendSync(cmd, data) {
+  sendSync(cmd, data, context) {
     let res;
-    postWithCallback(cmd, data, payload => { res = payload; });
+    postWithCallback(cmd, data, context, payload => { res = payload; });
     return res;
   },
 };
 
-function postWithCallback(cmd, data, cb) {
+function postWithCallback(cmd, data, context, cb) {
   const id = getUniqId();
   callbacks[id] = (payload) => {
     delete callbacks[id];
     cb(payload);
   };
-  bridge.post(cmd, { callbackId: id, payload: data });
+  bridge.post(cmd, { callbackId: id, payload: data }, context);
 }
 
 export default bridge;

src/background/utils/db.js
@@ -42,6 +42,12 @@ Object.assign(commands, {
   GetScriptCode(id) {
     return storage.code.getOne(id);
   },
+  GetScriptVer(opts) {
+    const script = getScript(opts);
+    return script && !script.config.removed
+      ? script.meta.version
+      : null;
+  },
   /** @return {Promise<void>} */
   MarkRemoved({ id, removed }) {
     return updateScriptInfo(id, {
@@ -139,7 +145,7 @@ preInitialize.push(async () => {
       // listing all known resource urls in order to remove unused mod keys
       const {
         custom: { pathMap = {} } = {},
-        meta = {},
+        meta = script.meta = {},
       } = script;
       meta.grant = [...new Set(meta.grant || [])]; // deduplicate
       meta.require?.forEach(rememberUrl, pathMap);
@@ -290,7 +296,7 @@ export async function getScriptsByURL(url, isTop) {
     const runAt = `${custom.runAt || meta.runAt || ''}`.match(RUN_AT_RE)?.[1] || 'end';
     const env = runAt === 'start' || runAt === 'body' ? envStart : envDelayed;
     env.ids.push(id);
-    if (meta.grant?.some(GMVALUES_RE.test, GMVALUES_RE)) {
+    if (meta.grant.some(GMVALUES_RE.test, GMVALUES_RE)) {
       env[ENV_VALUE_IDS].push(id);
     }
     for (const [list, name] of [

src/injected/content/clipboard.js
@@ -1,43 +1,32 @@
-import { sendCmd } from '#/common';
-import { logging } from '../utils/helpers';
+import { log } from '../utils/helpers';
 import bridge from './bridge';
 
-// old Firefox defines it on a different prototype so we'll just grab it from document directly
-const { execCommand } = document;
-const { setData } = DataTransfer[Prototype];
-const { get: getClipboardData } = describeProperty(ClipboardEvent[Prototype], 'clipboardData');
-const { preventDefault, stopImmediatePropagation } = Event[Prototype];
-
-let clipboardData;
-
-bridge.addHandlers({
-  __proto__: null, // Object.create(null) may be spoofed
-  SetClipboard(data) {
-    if (bridge.isFirefox) {
-      // Firefox does not support copy from background page.
-      // ref: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Interact_with_the_clipboard
-      // The dirty way will create a <textarea> element in web page and change the selection.
-      setClipboard(data);
-    } else {
-      sendCmd('SetClipboard', data);
-    }
-  },
-});
-
-function onCopy(e) {
-  e::stopImmediatePropagation();
-  e::preventDefault();
-  const { type, data } = clipboardData;
-  e::getClipboardData()::setData(type || 'text/plain', data);
-}
-
-function setClipboard({ type, data }) {
-  clipboardData = { type, data };
-  document::addEventListener('copy', onCopy, false);
-  const ret = document::execCommand('copy', false, null);
-  document::removeEventListener('copy', onCopy, false);
-  clipboardData = null;
-  if (process.env.DEBUG && !ret) {
-    logging.warn('Copy failed!');
+bridge.onScripts.push(() => {
+  let setClipboard;
+  if (bridge.isFirefox) {
+    let clipboardData;
+    // old Firefox defines it on a different prototype so we'll just grab it from document directly
+    const { execCommand } = document;
+    const { setData } = DataTransfer[Prototype];
+    const { get: getClipboardData } = describeProperty(ClipboardEvent[Prototype], 'clipboardData');
+    const { preventDefault, stopImmediatePropagation } = Event[Prototype];
+    const onCopy = e => {
+      e::stopImmediatePropagation();
+      e::preventDefault();
+      e::getClipboardData()::setData(clipboardData.type || 'text/plain', clipboardData.data);
+    };
+    setClipboard = params => {
+      clipboardData = params;
+      document::addEventListener('copy', onCopy);
+      if (!document::execCommand('copy') && process.env.DEBUG) {
+        log('warn', null, 'GM_setClipboard failed!');
+      }
+      document::removeEventListener('copy', onCopy);
+      clipboardData = null;
+    };
   }
-}
+  bridge.addHandlers({
+    __proto__: null, // Object.create(null) may be spoofed
+    SetClipboard: setClipboard || true,
+  }, true);
+});

src/injected/content/index.js
@@ -37,17 +37,38 @@ const { split } = '';
   const data = IS_FIREFOX && Event[Prototype].composedPath
     ? await getDataFF(dataPromise)
     : await dataPromise;
+  const { allow } = bridge;
   // 1) bridge.post may be overridden in injectScripts
   // 2) cloneInto is provided by Firefox in content scripts to expose data to the page
-  bridge.post = bindEvents(contentId, webId, bridge.onHandle, global.cloneInto);
+  bindEvents(contentId, webId, bridge, global.cloneInto);
+  bridge.contentId = contentId;
   bridge.ids = data.ids;
   bridge.isFirefox = data.info.isFirefox;
   bridge.injectInto = data.injectInto;
   isPopupShown = data.isPopupShown;
-  if (data.expose) bridge.post('Expose');
-  if (data.scripts) await injectScripts(contentId, webId, data, isXml);
+  if (data.expose) {
+    allow('GetScriptVer', contentId);
+    bridge.addHandlers({ GetScriptVer: true }, true);
+    bridge.post('Expose');
+  }
+  if (data.scripts) {
+    bridge.onScripts.forEach(fn => fn());
+    allow('SetTimeout', contentId);
+    allow('InjectList', contentId);
+    allow('Pong', contentId);
+    await injectScripts(contentId, webId, data, isXml);
+  }
+  bridge.onScripts = null;
   sendSetPopup();
 })().catch(IS_FIREFOX && console.error); // Firefox can't show exceptions in content scripts
+
+bridge.addBackgroundHandlers({
+  __proto__: null, // Object.create(null) may be spoofed
+  PopupShown(state) {
+    isPopupShown = state;
+    sendSetPopup();
+  },
+}, true);
 
 bridge.addBackgroundHandlers({
   __proto__: null, // Object.create(null) may be spoofed
@@ -55,10 +76,6 @@ bridge.addBackgroundHandlers({
     const id = +data[0]::split(':', 1)[0];
     const realm = invokableIds::includes(id) && INJECT_CONTENT;
     bridge.post('Command', data, realm);
-  },
-  PopupShown(state) {
-    isPopupShown = state;
-    sendSetPopup();
   },
   UpdatedValues(data) {
     const dataPage = createNullObj();
@@ -73,7 +90,6 @@ bridge.addBackgroundHandlers({
 
 bridge.addHandlers({
   __proto__: null, // Object.create(null) may be spoofed
-  UpdateValue: sendCmd,
   RegisterMenu(data) {
     if (IS_TOP) {
       const id = data[0];
@@ -108,9 +124,9 @@ bridge.addHandlers({
       return e.stack;
     }
   },
-  GetScript: sendCmd,
-  SetTimeout: sendCmd,
-  TabFocus: sendCmd,
+  SetTimeout: true,
+  TabFocus: true,
+  UpdateValue: true,
 });
 
 async function sendSetPopup(isDelayed) {

src/injected/content/inject.js
@@ -5,14 +5,15 @@ import { elemByTag, NS_HTML, log } from '../utils/helpers';
 import bridge from './bridge';
 
 // Firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1408996
-const VMInitInjection = window[process.env.INIT_FUNC_NAME];
+let VMInitInjection = window[process.env.INIT_FUNC_NAME];
 // To avoid running repeatedly due to new `document.documentElement`
 // (the prop is undeletable so a userscript can't fool us on reinjection)
 defineProperty(window, process.env.INIT_FUNC_NAME, { value: 1 });
 
+const regexpTest = RegExp[Prototype].test;
 const stringIncludes = ''.includes;
 const resolvedPromise = Promise.resolve();
-const { runningIds } = bridge;
+const { allow, runningIds } = bridge;
 let contLists;
 let pgLists;
 /** @type {Object<string,VMInjectionRealm>} */
@@ -108,6 +109,7 @@ export async function injectScripts(contentId, webId, data, isXml) {
       const realmData = realms[realm];
       realmData.lists[script.runAt].push(script); // 'start' or 'body' per getScriptsByURL()
       realmData.is = true;
+      allowCommands(script);
     } else {
       bridge.failedIds.push(id);
     }
@@ -168,6 +170,7 @@ async function injectDelayedScripts(contentId, webId, { cache, scripts }, getRea
   if (needsInvoker && contentId) {
     setupContentInvoker(contentId, webId);
   }
+  scripts::forEach(allowCommands);
   injectAll('end');
   injectAll('idle');
 }
@@ -177,7 +180,7 @@ function checkInjectable() {
     Pong() {
       pageInjectable = true;
     },
-  });
+  }, true);
   bridge.post('Ping');
   return pageInjectable;
 }
@@ -264,4 +267,40 @@ function setupContentInvoker(contentId, webId) {
   bridge.post = (cmd, params, realm) => (
     (realm === INJECT_CONTENT ? invokeContent : postViaBridge)(cmd, params)
   );
+  VMInitInjection = null; // release for GC
+}
+
+/**
+ * @param {VMInjectedScript | VMScript} script
+ */
+function allowCommands(script) {
+  const { dataKey } = script;
+  allow('Run', dataKey);
+  script.meta.grant::forEach(grant => {
+    const gm = /^GM[._]/::regexpTest(grant) && grant::slice(3);
+    if (grant === 'GM_xmlhttpRequest' || grant === 'GM.xmlHttpRequest' || gm === 'download') {
+      allow('AbortRequest', dataKey);
+      allow('HttpRequest', dataKey);
+    } else if (grant === 'window.close') {
+      allow('TabClose', dataKey);
+    } else if (grant === 'window.focus') {
+      allow('TabFocus', dataKey);
+    } else if (gm === 'addElement' || gm === 'addStyle') {
+      allow('AddElement', dataKey);
+    } else if (gm === 'setValue' || gm === 'deleteValue') {
+      allow('UpdateValue', dataKey);
+    } else if (gm === 'notification') {
+      allow('Notification', dataKey);
+      allow('RemoveNotification', dataKey);
+    } else if (gm === 'openInTab') {
+      allow('TabOpen', dataKey);
+      allow('TabClose', dataKey);
+    } else if (gm === 'registerMenuCommand') {
+      allow('RegisterMenu', dataKey);
+    } else if (gm === 'setClipboard') {
+      allow('SetClipboard', dataKey);
+    } else if (gm === 'unregisterMenuCommand') {
+      allow('UnregisterMenu', dataKey);
+    }
+  });
 }

src/injected/content/requests.js
@@ -16,7 +16,7 @@ bridge.addHandlers({
     };
     sendCmd('HttpRequest', opts);
   },
-  AbortRequest: sendCmd,
+  AbortRequest: true,
 });
 
 bridge.addBackgroundHandlers({

src/injected/utils/index.js
@@ -1,11 +1,11 @@
-export function bindEvents(srcId, destId, handle, cloneInto) {
-  const getDetail = describeProperty(CustomEvent[Prototype], 'detail').get;
-  const pageContext = cloneInto && document.defaultView;
-  document::addEventListener(srcId, e => handle(e::getDetail()));
-  return (cmd, params) => {
-    const data = { cmd, data: params };
-    const detail = cloneInto ? cloneInto(data, pageContext) : data;
+const getDetail = describeProperty(CustomEvent[Prototype], 'detail').get;
+
+export function bindEvents(srcId, destId, bridge, cloneInto) {
+  global::addEventListener(srcId, e => bridge.onHandle(e::getDetail()));
+  bridge.post = (cmd, params, context) => {
+    const data = { cmd, data: params, dataKey: (context || bridge).dataKey };
+    const detail = cloneInto ? cloneInto(data, document) : data;
     const e = new CustomEvent(destId, { detail });
-    document::dispatchEvent(e);
+    global::dispatchEvent(e);
   };
 }

commit 4272f71f866fdddbc148fb0b1e70b08ddf41e949

fix: allow GM_getResourceXXX, regressed in 6dac9329

src/injected/content/bridge.js
@@ -2,6 +2,7 @@ import { isPromise, sendCmd } from '#/common';
 import { INJECT_PAGE, browser } from '#/common/consts';
 
 const allowed = createNullObj();
+const dataKeyNameMap = createNullObj();
 const handlers = createNullObj();
 const bgHandlers = createNullObj();
 const onScripts = [];
@@ -14,6 +15,34 @@ const assignHandlers = (dest, src, force) => {
 };
 const allowCmd = (cmd, dataKey) => {
   (allowed[cmd] || (allowed[cmd] = createNullObj()))[dataKey] = true;
+};
+const XHR = ['HttpRequest', 'AbortRequest'];
+const ADD_ELEMENT = ['AddElement'];
+const UPDATE_VALUE = ['UpdateValue'];
+const TAB_CLOSE = ['TabClose'];
+const GET_RESOURCE = ['GetResource'];
+const GM_CMD = {
+  __proto__: null,
+  addElement: ADD_ELEMENT,
+  addStyle: ADD_ELEMENT,
+  deleteValue: UPDATE_VALUE,
+  download: XHR,
+  getResourceText: GET_RESOURCE,
+  getResourceURL: GET_RESOURCE, // also allows the misspelled GM.getResourceURL for compatibility
+  notification: ['Notification', 'RemoveNotification'],
+  openInTab: ['TabOpen', TAB_CLOSE],
+  registerMenuCommand: ['RegisterMenu'],
+  setClipboard: ['SetClipboard'],
+  setValue: UPDATE_VALUE,
+  unregisterMenuCommand: ['UnregisterMenu'],
+};
+const GRANT_CMD = {
+  __proto__: null,
+  'GM.getResourceUrl': GET_RESOURCE,
+  'GM.xmlHttpRequest': XHR,
+  'GM_xmlhttpRequest': XHR, // eslint-disable-line quote-props
+  [WINDOW_CLOSE]: TAB_CLOSE,
+  [WINDOW_FOCUS]: ['TabFocus'],
 };
 
 const bridge = {
@@ -46,39 +75,19 @@ const bridge = {
    */
   allowScript({ dataKey, meta }) {
     allowCmd('Run', dataKey);
+    dataKeyNameMap[dataKey] = meta.name;
     meta.grant::forEach(grant => {
-      const gm = /^GM[._]/::regexpTest(grant) && grant::slice(3);
-      if (grant === 'GM_xmlhttpRequest' || grant === 'GM.xmlHttpRequest' || gm === 'download') {
-        allowCmd('AbortRequest', dataKey);
-        allowCmd('HttpRequest', dataKey);
-      } else if (grant === 'window.close') {
-        allowCmd('TabClose', dataKey);
-      } else if (grant === 'window.focus') {
-        allowCmd('TabFocus', dataKey);
-      } else if (gm === 'addElement' || gm === 'addStyle') {
-        allowCmd('AddElement', dataKey);
-      } else if (gm === 'setValue' || gm === 'deleteValue') {
-        allowCmd('UpdateValue', dataKey);
-      } else if (gm === 'notification') {
-        allowCmd('Notification', dataKey);
-        allowCmd('RemoveNotification', dataKey);
-      } else if (gm === 'openInTab') {
-        allowCmd('TabOpen', dataKey);
-        allowCmd('TabClose', dataKey);
-      } else if (gm === 'registerMenuCommand') {
-        allowCmd('RegisterMenu', dataKey);
-      } else if (gm === 'setClipboard') {
-        allowCmd('SetClipboard', dataKey);
-      } else if (gm === 'unregisterMenuCommand') {
-        allowCmd('UnregisterMenu', dataKey);
-      }
+      const cmds = GRANT_CMD[grant]
+        || /^GM[._]/::regexpTest(grant) && GM_CMD[grant::slice(3)];
+      if (cmds) cmds::forEach(cmd => allowCmd(cmd, dataKey));
     });
   },
   // realm is provided when called directly via invokeHost
   async onHandle({ cmd, data, dataKey, node }, realm) {
     const handle = handlers[cmd];
     if (!handle || !allowed[cmd]?.[dataKey]) {
-      throw new ErrorSafe(`[Violentmonkey] Invalid command: "${cmd}" on ${global.location.host}`);
+      // TODO: maybe remove this check if our VAULT is reliable
+      log('info', null, `Invalid command: "${cmd}" from "${dataKeyNameMap[dataKey] || '?'}"`);
     }
     const callbackId = data && getOwnProp(data, CALLBACK_ID);
     if (callbackId) {

src/injected/web/gm-api-wrapper.js
@@ -56,10 +56,10 @@ export function makeGmApiWrapper(script) {
     componentUtils = makeComponentUtils();
   }
   assign(gm, componentUtils);
-  if (grant::indexOf('window.close') >= 0) {
+  if (grant::indexOf(WINDOW_CLOSE) >= 0) {
     gm.close = vmOwnFunc(() => bridge.post('TabClose', 0, context));
   }
-  if (grant::indexOf('window.focus') >= 0) {
+  if (grant::indexOf(WINDOW_FOCUS) >= 0) {
     gm.focus = vmOwnFunc(() => bridge.post('TabFocus', 0, context));
   }
   if (!gmApi && grant.length) gmApi = makeGmApi();