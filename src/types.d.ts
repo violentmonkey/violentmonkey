8af25a81caa62d6bee56fbbdd84b5ee7313cdf03
tophf <tophf@gmx.com>
2022-10-01 00:47:36 +0300

    fix: TS warnings in types

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,7 +3,7 @@
 declare type NumBool = 0 | 1
 /** null means "default" or "inherit from global" */
 declare type NumBoolNull = 0 | 1 | null
-declare type StringMap = Object<string,string>
+declare type StringMap = { [key: string]: string }
 
 //#endregion Generic
 //#region GM-specific
@@ -12,12 +12,12 @@ declare type StringMap = Object<string,string>
  * Script context object used by GM### API
  */
 declare interface GMContext {
-  async?: boolean
-  dataKey: string
-  id: number
-  resCache: StringMap
-  resources: StringMap
-  script: VMScript
+  async?: boolean;
+  dataKey: string;
+  id: number;
+  resCache: StringMap;
+  resources: StringMap;
+  script: VMScript;
 }
 /**
  * GM_xmlhttpRequest paraphernalia
@@ -25,76 +25,76 @@ declare interface GMContext {
 declare namespace GMReq {
   type EventType = keyof XMLHttpRequestEventMap;
   type UserOpts = VMScriptGMDownloadOptions | VMScriptGMXHRDetails;
-  declare interface BG {
-    anonymous: boolean
-    blobbed: boolean
-    cb: function(Object)
-    chunked: boolean
-    coreId: number
-    eventsToNotify: string[]
-    frameId: number
-    id: string
-    noNativeCookie: boolean
-    responseHeaders: string
-    storeId: string
-    tabId: number
-    url: string
-    xhr: XMLHttpRequest
+  interface BG {
+    anonymous: boolean;
+    blobbed: boolean;
+    cb: Function;
+    chunked: boolean;
+    coreId: number;
+    eventsToNotify: string[];
+    frameId: number;
+    id: string;
+    noNativeCookie: boolean;
+    responseHeaders: string;
+    storeId: string;
+    tabId: number;
+    url: string;
+    xhr: XMLHttpRequest;
   }
-  declare interface Content {
-    realm: VMScriptInjectInto
-    wantsBlob: boolean
-    eventsToNotify: EventType[]
-    fileName: string
-    arr?: Uint8Array
-    resolve?: function
-    dataSize?: number
-    contentType?: string
-    gotChunks?: boolean
+  interface Content {
+    realm: VMScriptInjectInto;
+    wantsBlob: boolean;
+    eventsToNotify: EventType[];
+    fileName: string;
+    arr?: Uint8Array;
+    resolve?: Function;
+    dataSize?: number;
+    contentType?: string;
+    gotChunks?: boolean;
   }
-  declare interface Web {
-    id: string
-    scriptId: number
-    opts: UserOpts
-    raw: string | Blob | ArrayBuffer
-    response: string | Blob | ArrayBuffer
-    headers: string
-    text?: string
+  interface Web {
+    id: string;
+    scriptId: number;
+    opts: UserOpts;
+    raw: string | Blob | ArrayBuffer;
+    response: string | Blob | ArrayBuffer;
+    headers: string;
+    text?: string;
   }
-  declare namespace Message {
+  namespace Message {
     type Chunk = {
-      pos: number
-      data: string
-      last: boolean
+      pos: number;
+      data: string;
+      last: boolean;
     }
     /** From background */
-    declare interface BG {
-      blobbed: boolean
-      chunked: boolean
-      contentType: string
-      data: VMScriptResponseObject
-      dataSize: number
-      id: string
-      numChunks?: number
-      type: EventType
-      chunk?: Chunk
+    interface BG {
+      blobbed: boolean;
+      chunked: boolean;
+      contentType: string;
+      data: VMScriptResponseObject;
+      dataSize: number;
+      id: string;
+      numChunks?: number;
+      type: EventType;
+      chunk?: Chunk;
     }
     /** From web/content bridge */
-    declare interface Web {
-      id: string
-      scriptId: number
-      anonymous: boolean
-      fileName: string
-      data: Array
-      eventsToNotify: EventType[]
-      headers?: StringMap
-      method?: string
-      overrideMimeType?: string
-      password?: string
-      timeout?: number
-      url: string
-      user?: string
-      xhrType: XMLHttpRequestResponseType
+    interface Web {
+      id: string;
+      scriptId: number;
+      anonymous: boolean;
+      fileName: string;
+      data: any[];
+      eventsToNotify: EventType[];
+      headers?: StringMap;
+      method?: string;
+      overrideMimeType?: string;
+      password?: string;
+      timeout?: number;
+      url: string;
+      user?: string;
+      xhrType: XMLHttpRequestResponseType;
     }
   }
 }
@@ -107,143 +107,144 @@ declare type VMBadgeMode = 'unique' | 'total' | ''
  * Internal script representation
  */
 declare interface VMScript {
-  config: VMScript.Config
-  custom: VMScript.Custom
-  meta: VMScript.Meta
-  props: VMScript.Props
+  config: VMScript.Config;
+  custom: VMScript.Custom;
+  meta: VMScript.Meta;
+  props: VMScript.Props;
 }
 declare namespace VMScript {
-  declare type Config = {
-    enabled: NumBool
-    removed: NumBool
-    shouldUpdate: NumBool
-    notifyUpdates?: NumBoolNull
+  type Config = {
+    enabled: NumBool;
+    removed: NumBool;
+    shouldUpdate: NumBool;
+    notifyUpdates?: NumBoolNull;
   }
-  declare type Custom = {
-    name?: string
-    downloadURL:? string
-    homepageURL?: string
-    lastInstallURL?: string
-    updateURL?: string
-    injectInto?: VMScriptInjectInto
-    noframes?: NumBoolNull
-    exclude?: string[]
-    excludeMatch?: string[]
-    include?: string[]
-    match?: string[]
-    origExclude: boolean
-    origExcludeMatch: boolean
-    origInclude: boolean
-    origMatch: boolean
-    pathMap?: StringMap
-    runAt?: VMScriptRunAt
+  type Custom = {
+    name?: string;
+    downloadURL?: string;
+    homepageURL?: string;
+    lastInstallURL?: string;
+    updateURL?: string;
+    injectInto?: VMScriptInjectInto;
+    noframes?: NumBoolNull;
+    exclude?: string[];
+    excludeMatch?: string[];
+    include?: string[];
+    match?: string[];
+    origExclude: boolean;
+    origExcludeMatch: boolean;
+    origInclude: boolean;
+    origMatch: boolean;
+    pathMap?: StringMap;
+    runAt?: VMScriptRunAt;
   }
-  declare type Meta = {
-    description?: string
-    downloadURL?: string
-    exclude: string[]
-    excludeMatch: string[]
-    grant: string[]
-    homepageURL?: string
-    icon?: string
-    include: string[]
-    injectInto?: VMScriptInjectInto
-    match: string[]
-    namespace?: string
-    name: string
-    noframes?: boolean
-    require: string[]
-    resources: StringMap
-    runAt?: VMScriptRunAt
-    supportURL?: string
-    unwrap?: boolean
-    version?: string
+  type Meta = {
+    description?: string;
+    downloadURL?: string;
+    exclude: string[];
+    excludeMatch: string[];
+    grant: string[];
+    homepageURL?: string;
+    icon?: string;
+    include: string[];
+    injectInto?: VMScriptInjectInto;
+    match: string[];
+    namespace?: string;
+    name: string;
+    noframes?: boolean;
+    require: string[];
+    resources: StringMap;
+    runAt?: VMScriptRunAt;
+    supportURL?: string;
+    unwrap?: boolean;
+    version?: string;
   }
-  declare type Props = {
-    id: number
-    lastModified: number
-    lastUpdated: number
-    position: number
-    uri: string
-    uuid: string
+  type Props = {
+    id: number;
+    lastModified: number;
+    lastUpdated: number;
+    position: number;
+    uri: string;
+    uuid: string;
   }
 }
 /**
  * Injection data sent to the content bridge
  */
 declare interface VMInjection {
-  expose: string | false
-  scripts: VMInjection.Script[]
-  injectInto: VMScriptInjectInto
-  injectPage: boolean
-  cache: StringMap
+  expose: string | false;
+  scripts: VMInjection.Script[];
+  injectInto: VMScriptInjectInto;
+  injectPage: boolean;
+  cache: StringMap;
   feedId: {
     /** InjectionFeedback cache key for cleanup when getDataFF outruns GetInjected */
-    cacheKey: string
+    cacheKey: string;
     /** InjectionFeedback cache key for envDelayed */
-    envKey: string
+    envKey: string;
   }
   /** tells content bridge to expect envDelayed */
-  hasMore: boolean
+  hasMore: boolean;
   /** content bridge adds the actually running ids and sends via SetPopup */
-  ids: number[]
-  info: VMInjection.Info
-  isPopupShown?: boolean
+  ids: number[];
+  info: VMInjection.Info;
+  isPopupShown?: boolean;
 }
 /**
  * Injection paraphernalia in the background script
  */
 declare namespace VMInjection {
-  declare interface Env {
-    cache: StringMap
-    cacheKeys: string[]
-    code: StringMap
-    depsMap: Object<string,number[]>
+  interface Env {
+    cache: StringMap;
+    cacheKeys: string[];
+    code: StringMap;
+    /** Dependencies by key to script ids */
+    depsMap: { [url: string]: number[] };
     /** Only present in envStart */
-    disabledIds?: number[]
+    disabledIds?: number[];
     /** Only present in envStart */
-    envDelayed?: Env
-    ids: number[]
-    promise: Promise<Env>
-    reqKeys: string[]
-    require: StringMap
-    scripts: VMScript[]
-    sizing?: boolean
-    value: Object<string,StringMap>
-    valueIds: number[]
+    envDelayed?: Env;
+    ids: number[];
+    promise: Promise<Env>;
+    reqKeys: string[];
+    require: StringMap;
+    scripts: VMScript[];
+    sizing?: boolean;
+    value: { [scriptId: string]: StringMap };
+    valueIds: number[];
   }
   /**
    * Contains the injected data and non-injected auxiliaries
    */
-  declare interface Bag {
-    inject: VMInjection
-    feedback: (string|number)[] | false
-    csar: Promise<browser.contentScripts.RegisteredContentScript>
+  interface Bag {
+    inject: VMInjection;
+    feedback: (string|number)[] | false;
+    csar: Promise<browser.contentScripts.RegisteredContentScript>;
   }
-  declare interface Info {
-    ua: VMScriptGMInfoPlatform
+  interface Info {
+    ua: VMScriptGMInfoPlatform;
   }
   /**
    * Script prepared for injection
    */
-  declare interface Script extends VMScript {
-    dataKey: string
-    displayName: string
-    code: string
-    metaStr: string
-    runAt?: 'start' | 'body' | 'end' | 'idle'
-    values?: StringMap
+  interface Script extends VMScript {
+    dataKey: string;
+    displayName: string;
+    code: string;
+    metaStr: string;
+    runAt?: 'start' | 'body' | 'end' | 'idle';
+    values?: StringMap;
   }
 }
 declare interface VMRealmData {
   lists: {
-    start: VMScript[]
-    body: VMScript[]
-    end: VMScript[]
-    idle: VMScript[]
+    start: VMScript[];
+    body: VMScript[];
+    end: VMScript[];
+    idle: VMScript[];
   }
-  is: boolean
-  info: Sent
+  is: boolean;
+  info: VMInjection.Info;
 }
 /**
  * Internal request()
@@ -251,28 +252,28 @@ declare interface VMRealmData {
 declare namespace VMReq {
   interface Options extends RequestInit {
     /** @implements XMLHttpRequestResponseType */
-    responseType: '' | 'arraybuffer' | 'blob' | 'json' | 'text'
+    responseType: '' | 'arraybuffer' | 'blob' | 'json' | 'text';
   }
-  declare interface Response {
-    url: string,
-    status: number,
-    headers: Headers,
-    data: string | ArrayBuffer | Blob | Object
+  interface Response {
+    url: string;
+    status: number;
+    headers: Headers;
+    data: string | ArrayBuffer | Blob | Object;
   }
 }
 declare type VMSearchOptions = {
-  reversed?: boolean
-  wrapAround?: chrome.tabs.Tab
-  reuseCursor?: boolean
-  pos?: { line: number, ch: number }
+  reversed?: boolean;
+  wrapAround?: chrome.tabs.Tab;
+  reuseCursor?: boolean;
+  pos?: { line: number, ch: number };
 }
 declare interface VMUserAgent extends VMScriptGMInfoPlatform {
   /** Chrome/ium version number */
-  chrome: number | NaN
+  chrome: number | typeof NaN;
   /** derived from UA string initially, a real number when `ready` */
-  firefox: number | NaN
+  firefox: number | typeof NaN;
   /** resolves when `browser` API returns real versions */
-  ready: Promise<void>
+  ready: Promise<void>;
 }
 
 //#endregion Generic
