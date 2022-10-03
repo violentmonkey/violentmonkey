b2827605c825d466e7a11e240a3de9ab149096c6
tophf <tophf@gmx.com>
2022-10-04 01:37:18 +0300

    refactor: pass exceptions from bg option hooks to caller
    + method shorthands in common::options to avoid duplicate setOption/getOption in bg and simpler jumping to usage/source in IDE

--- src/common/options.js
+++ src/common/options.js
@@ -3,38 +3,32 @@ import { initHooks, sendCmdDirectly } from '.';
 import { forEachEntry, objectGet, objectSet } from './object';
 
 let options = {};
-const hooks = initHooks();
+const { hook, fire } = initHooks();
 const ready = sendCmdDirectly('GetAllOptions', null, { retry: true })
 .then((data) => {
   options = data;
-  if (data) hooks.fire(data);
+  if (data) fire(data);
 });
-
-function getOption(key) {
-  return objectGet(options, key) ?? objectGet(defaults, key);
-}
-
-function setOption(key, value) {
-  // the updated options object will be propagated from the background script after a pause
-  // so meanwhile the local code should be able to see the new value using options.get()
-  objectSet(options, key, value);
-  sendCmdDirectly('SetOptions', { key, value });
-}
-
-function updateOptions(data) {
-  // Keys in `data` may be { flattened.like.this: 'foo' }
-  const expandedData = {};
-  data::forEachEntry(([key, value]) => {
-    objectSet(options, key, value);
-    objectSet(expandedData, key, value);
-  });
-  hooks.fire(expandedData);
-}
 
 export default {
   ready,
-  get: getOption,
-  set: setOption,
-  update: updateOptions,
-  hook: hooks.hook,
+  hook,
+  get(key) {
+    return objectGet(options, key) ?? objectGet(defaults, key);
+  },
+  set(key, value) {
+    // the updated options object will be propagated from the background script after a pause
+    // so meanwhile the local code should be able to see the new value using options.get()
+    objectSet(options, key, value);
+    return sendCmdDirectly('SetOptions', { key, value, reply: true });
+  },
+  update(data) {
+    // Keys in `data` may be { flattened.like.this: 'foo' }
+    const expandedData = {};
+    data::forEachEntry(([key, value]) => {
+      objectSet(options, key, value);
+      objectSet(expandedData, key, value);
+    });
+    fire(expandedData);
+  },
 };
