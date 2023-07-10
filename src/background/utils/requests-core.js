violentmonkey/violentmonkey

commit 48c5762afd6464e066ed3b0d498eb79fd4f0ee2f

fix: append custom cookie in GMxhr
fixes #1720

src/background/utils/requests-core.js
@@ -47,10 +47,10 @@ const EXTRA_HEADERS = [
 const headersToInject = {};
 /** @param {chrome.webRequest.HttpHeader} header */
 const isVmVerify = header => header.name === VM_VERIFY;
-const isNotCookie = header => !/^cookie2?$/i.test(header.name);
+export const isCookie = header => /^cookie2?$/i.test(header.name);
 const isSendable = header => !isVmVerify(header)
   && !(/^origin$/i.test(header.name) && header.value === extensionOrigin);
-const isSendableAnon = header => isSendable(header) && isNotCookie(header);
+const isSendableAnon = header => isSendable(header) && !isCookie(header);
 const SET_COOKIE_RE = /^set-cookie2?$/i;
 const SET_COOKIE_VALUE_RE = re`
   /^\s*  (?:__(Secure|Host)-)?  ([^=\s]+)  \s*=\s*  (")?  ([!#-+\--:<-[\]-~]*)  \3(.*)  /x`;
@@ -95,8 +95,15 @@ function onBeforeSendHeaders({ requestHeaders: headers, requestId, url }) {
     verify[requestId] = reqId;
     req.coreId = requestId;
     req.url = url; // remember redirected URL with #hash as it's stripped in XHR.responseURL
-    headers = (req.noNativeCookie ? headers.filter(isNotCookie) : headers)
-    .concat(headersToInject[reqId] || [])
+    const headers2 = headersToInject[reqId] || [];
+    const i = headers.findIndex(isCookie);
+    const j = headers2.findIndex(isCookie);
+    if (req.noNativeCookie) {
+      headers.splice(i, 1);
+    } else if (i >= 0 && j >= 0) {
+      headers[i].value += '; ' + headers2.splice(j, 1)[0].value;
+    }
+    headers = headers.concat(headers2)
     .filter(req.anonymous ? isSendableAnon : isSendable);
   }
   return { requestHeaders: headers };

src/background/utils/requests.js
@@ -4,7 +4,7 @@ import ua from '@/common/ua';
 import cache from './cache';
 import { addPublicCommands, commands } from './message';
 import {
-  FORBIDDEN_HEADER_RE, VM_VERIFY, requests, toggleHeaderInjector, verify,
+  FORBIDDEN_HEADER_RE, VM_VERIFY, isCookie, requests, toggleHeaderInjector, verify,
 } from './requests-core';
 
 addPublicCommands({
@@ -197,7 +197,7 @@ async function httpRequest(opts, events, src, cb) {
       xhr.setRequestHeader(name, value);
     }
     if (shouldSendCookies) {
-      shouldSendCookies = !/^cookie$/i.test(name);
+      shouldSendCookies = !isCookie({ name });
     }
   });
   xhr[kResponseType] = willStringifyBinaries && 'blob' || xhrType || 'text';

commit 9b7b5067e04fffbbff1fac014d4d1b1c249b3527

fix: send custom cookie header in anonymous gmxhr
regressed in 48c5762a
fixes #1848

src/background/utils/requests-core.js
@@ -107,8 +107,9 @@ function onBeforeSendHeaders({ requestHeaders: headers, requestId, url }) {
         headers[i].value += '; ' + headers2.splice(j, 1)[0].value;
       }
     }
-    headers = headers.concat(headers2)
-    .filter(req.anonymous ? isSendableAnon : isSendable);
+    headers = headers
+    .filter(req.anonymous ? isSendableAnon : isSendable)
+    .concat(headers2);
   }
   return { requestHeaders: headers };
 }