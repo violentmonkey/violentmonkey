73f0e5fdae4a782c71d3f9155b18b038af9ec59f
Gerald <code@gera2ld.space>
2024-02-13 14:25:47 +0800

    fix: style

--- src/options/views/tab-installed.vue
+++ src/options/views/tab-installed.vue
@@ -56,7 +56,7 @@
             </Tooltip>
           </div>
         </template>
-        <div v-else v-text="i18n('headerRecycleBin')" />
+        <div v-else class="ml-2" v-text="i18n('headerRecycleBin')" />
         <div class="flex-auto"></div>
         <LocaleGroup i18n-key="labelFilterSort" class="ml-1">
           <select :value="filters.sort" @change="handleOrderChange" class="h-100">
