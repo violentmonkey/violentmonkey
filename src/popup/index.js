24c9dfb0c264cd014a200c6d96bd5e24cc4dc5fb
tophf <extophf@gmail.com>
2024-03-18 21:00:06 +0300

    fix: respond to SetPopup in the correct popup

--- src/popup/index.js
+++ src/popup/index.js
@@ -14,72 +14,73 @@ initialize();
 render(App);
 
 Object.assign(handlers, {
+  /** Must be synchronous to prevent the wrong visible popup from responding to the message */
   Run({ reset }, { [kFrameId]: frameId, tab }) {
     // The tab got reloaded so Run+reset comes right before SetPopup, see cmd-run.js
     if (reset && !frameId && isMyTab(tab)) {
       initialize();
     }
   },
-  async SetPopup(data, { [kFrameId]: frameId, tab, url }) {
-    if (!isMyTab(tab)) return;
-    /* SetPopup from a sub-frame may come first so we need to wait for the main page
-     * because we only show the iframe menu for unique scripts that don't run in the main page */
-    const isTop = frameId === 0;
-    let disabledIds;
-    if (!data.all) {
-      disabledIds = await sendCmdDirectly('GetDisabledIds', { url, [kTop]: isTop });
-      Object.assign(data[IDS], disabledIds);
+  /** Must be synchronous to prevent the wrong visible popup from responding to the message */
+  SetPopup(data, src) {
+    if (isMyTab(src.tab)) {
+      return setPopup(data, src);
     }
-    if (!isTop) await mutex;
-    else {
-      store[IS_APPLIED] = data[INJECT_INTO] !== 'off'; // isApplied at the time of GetInjected
-    }
-    // Ensuring top script's menu wins over a per-frame menu with different commands
-    store.commands = Object.assign(data.menus, !isTop && store.commands);
-    const idMapAllFrames = store.idMap;
-    const idMapMain = idMapAllFrames[0] || (idMapAllFrames[0] = {});
-    const idMapOld = idMapAllFrames[frameId] || (idMapAllFrames[frameId] = {});
-    const idMap = data[IDS]::mapEntry(null, (id, val) => val !== idMapOld[id] && id);
-    const ids = Object.keys(idMap).map(Number);
-    if (ids.length) {
-      Object.assign(idMapOld, idMap);
-      // frameScripts may be appended multiple times if iframes have unique scripts
-      const scope = store[isTop ? SCRIPTS : 'frameScripts'];
-      const metas = data[SCRIPTS]?.filter(({ props: { id } }) => ids.includes(id))
-        || (Object.assign(data, await sendCmdDirectly('GetData', { ids })))[SCRIPTS];
-      metas.forEach(script => {
-        loadScriptIcon(script, data);
-        const { id } = script.props;
-        const state = idMap[id];
-        const badRealm = state === ID_BAD_REALM;
-        const renderedScript = scope.find(({ props }) => props.id === id);
-        if (renderedScript) script = renderedScript;
-        else if (isTop || !(id in idMapMain)) {
-          scope.push(script);
-          if (isTop) { // removing script from frameScripts if it ran there before the main frame
-            const { frameScripts } = store;
-            const i = frameScripts.findIndex(({ props }) => props.id === id);
-            if (i >= 0) frameScripts.splice(i, 1);
-          }
-        }
-        script.runs = state === CONTENT || state === PAGE;
-        script.pageUrl = url; // each frame has its own URL
-        script.failed = badRealm || state === ID_INJECTING;
-        script.syntax = state === ID_INJECTING;
-        if (badRealm && !store.injectionFailure) {
-          store.injectionFailure = { fixable: data[INJECT_INTO] === PAGE };
-        }
-      });
-    }
-    if (isTop) mutexResolve(); // resolving at the end after all `await` above are settled
-    return disabledIds;
   },
 });
 
-/* Since new Chrome prints a warning when ::-webkit-details-marker is used,
- * we add it only for old Chrome, which is detected via feature added in 89. */
-if (!CSS.supports?.('list-style-type', 'disclosure-open')) {
-  document.styleSheets[0].insertRule('.excludes-menu ::-webkit-details-marker {display:none}');
+async function setPopup(data, { [kFrameId]: frameId, url }) {
+  /* SetPopup from a sub-frame may come first so we need to wait for the main page
+   * because we only show the iframe menu for unique scripts that don't run in the main page */
+  const isTop = frameId === 0;
+  let disabledIds;
+  if (!data.all) {
+    disabledIds = await sendCmdDirectly('GetDisabledIds', { url, [kTop]: isTop });
+    Object.assign(data[IDS], disabledIds);
+  }
+  if (!isTop) await mutex;
+  else {
+    store[IS_APPLIED] = data[INJECT_INTO] !== 'off'; // isApplied at the time of GetInjected
+  }
+  // Ensuring top script's menu wins over a per-frame menu with different commands
+  store.commands = Object.assign(data.menus, !isTop && store.commands);
+  const idMapAllFrames = store.idMap;
+  const idMapMain = idMapAllFrames[0] || (idMapAllFrames[0] = {});
+  const idMapOld = idMapAllFrames[frameId] || (idMapAllFrames[frameId] = {});
+  const idMap = data[IDS]::mapEntry(null, (id, val) => val !== idMapOld[id] && id);
+  const ids = Object.keys(idMap).map(Number);
+  if (ids.length) {
+    Object.assign(idMapOld, idMap);
+    // frameScripts may be appended multiple times if iframes have unique scripts
+    const { frameScripts } = store;
+    const scope = isTop ? store[SCRIPTS] : frameScripts;
+    const metas = data[SCRIPTS]?.filter(({ props: { id } }) => ids.includes(id))
+      || (Object.assign(data, await sendCmdDirectly('GetData', { ids })))[SCRIPTS];
+    metas.forEach(script => {
+      loadScriptIcon(script, data);
+      const { id } = script.props;
+      const state = idMap[id];
+      const badRealm = state === ID_BAD_REALM;
+      const renderedScript = scope.find(({ props }) => props.id === id);
+      if (renderedScript) script = renderedScript;
+      else if (isTop || !(id in idMapMain)) {
+        scope.push(script);
+        if (isTop) { // removing script from frameScripts if it ran there before the main frame
+          const i = frameScripts.findIndex(({ props }) => props.id === id);
+          if (i >= 0) frameScripts.splice(i, 1);
+        }
+      }
+      script.runs = state === CONTENT || state === PAGE;
+      script.pageUrl = url; // each frame has its own URL
+      script.failed = badRealm || state === ID_INJECTING;
+      script.syntax = state === ID_INJECTING;
+      if (badRealm && !store.injectionFailure) {
+        store.injectionFailure = { fixable: data[INJECT_INTO] === PAGE };
+      }
+    });
+  }
+  if (isTop) mutexResolve(); // resolving at the end after all `await` above are settled
+  return disabledIds;
 }
 
 function initMutex(delay = 100) {
