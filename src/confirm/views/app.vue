141e208b427632584767771a7e04c47bdcd8ae61
tophf <extophf@gmail.com>
2024-06-04 00:46:51 +0300

    fix: ensure space before hotkey

--- src/confirm/views/app.vue
+++ src/confirm/views/app.vue
@@ -647,7 +647,7 @@ $vertLayoutThresholdMinus1: 1800px;
       content: attr(data-verb) " + ";
     }
     &::after {
-      content: " (" attr(data-hotkey) ")";
+      content: "\a0(" attr(data-hotkey) ")";
       opacity: .75;
       font-weight: normal;
     }
