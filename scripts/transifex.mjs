44d0402c21c90a85141a7556b730bd19fceb1f5d
tophf <tophff@gmail.com>
2026-09-19 23:20:13 +0300

    fix ceb6d831: js-yaml no longer exports a namespace

--- scripts/transifex.mjs
+++ scripts/transifex.mjs
@@ -1,6 +1,6 @@
 import { readFile, writeFile, mkdir } from 'fs/promises';
 import spawn from 'cross-spawn';
-import yaml from 'js-yaml';
+import * as yaml from 'js-yaml';
 
 const PROJECT_ID = 'o:violentmonkey:p:violentmonkey-nex';
 const RESOURCE_ID = `${PROJECT_ID}:r:messagesjson`;
